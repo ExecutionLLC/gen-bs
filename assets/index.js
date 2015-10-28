90cd06ad3980c014c3813bfd24a5f5401b0abf5e
elenabaranova <e.elfairy@gmail.com>
2015-10-28 17:54:52 +0300

    typehead

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,68 @@
 $(function () {
+/*
     $('.toggle').click(function () {
         $('.nav-list').toggleClass('active');
     });
+*/
 
-    $(document).on('click', '#navbar li a, .nav-list li a, .navigation a', function (e) {
+    $(document).on('click', '#navbar a', function (e) {
         var href = $(this).attr('href');
         if (href === '#' || /^http.*/.test(href)) {
             return;
         }
+  
         e.preventDefault();
-        $('.nav-list').removeClass('active');
+        //$('.nav-list').removeClass('active');
         location.hash = href;
         $('iframe').attr('src', href);
-        initNavigation(href);
+       
     });
 
-    var href = location.hash.substring(1) || 'welcome.html';
+    var href = location.hash.substring(1) || 'gen-index.html';
     $('iframe').attr('src', href);
-    initNavigation(href);
+    //initNavigation(href);
 
+/*
     $(window).on('blur',function() {
         $('.dropdown-toggle').parent().removeClass('open');
     });
+*/
+ 
+
+ var countries = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  // url points to a json file that contains an array of country names, see
+  // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
+  prefetch: '../json/samples1.json'
 });
 
+// passing in `null` for the `options` arguments will result in the default
+// options being used
+$('#prefetch .typeahead').typeahead(null, {
+  name: 'countries',
+  source: countries,
+  
+});
+/*
+      $('#samplesSelect').select2({
+            placeholder: "Find sample",
+            allowClear: true,
+            //minimumResultsForSearch: Infinity
+      });
+*/
+  
+
+    $('#btnOpenid').on('click', function (e) {
+      $('.dropdown-profile').removeClass("hidden");
+      $(this).addClass("hidden");
+     });
+     
+   
+     
+});
+
+/*
 function initNavigation(href) {
     var $el = $('a[href="' + href + '"]'),
         $prev, $next;
@@ -49,3 +88,4 @@ function initNavigation(href) {
             .find('span').text($next.text());
     }
 }
+*/
