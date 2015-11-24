9c92c5b85739c8d65687a0e34134141ab088ec8e
Vasily Loginov <loginov.vasily@gmail.com>
2015-11-24 23:03:29 +0600

    Now reading port from the environment.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express();
+app.set('port', process.env.PORT || 3456);
 app.use('/', express.static('public'));
 
-var server = app.listen(3456, function() {
+var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
   var port = server.address().port;
 
