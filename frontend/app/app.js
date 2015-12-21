bf007e1dc4b6aaea3b7a49beec437988a8d96810
elenabaranova <e.elfairy@gmail.com>
2015-12-21 11:34:38 +0300

    colors,state,messages

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -7,16 +7,17 @@ const $ = require('jquery');
 window.jQuery = $;
 window.$ = $;
 
-//import './assets/css/font-awesome-4.4.0/css/font-awesome.min.css';
-//import './assets/vendor/select2-4.0.1-rc.1/dist/css/select2.min.css';
-import './assets/vendor/jQuery-QueryBuilder-master/dist/css/query-builder.default.min.css';
+import './assets/vendor/jQuery-QueryBuilder/dist/css/query-builder.default.min.css';
 import './assets/css/bootstrap/js/bootstrap.js';
 import './assets/vendor/select2-4.0.1-rc.1/dist/js/select2.full.min.js';
+
 /*
 import './assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css';
 import './assets/vendor/malihu-custom-scrollbar-plugin/js/minified/jquery.mCustomScrollbar.min.js';
 import './assets/vendor/malihu-custom-scrollbar-plugin/js/minified/jquery.mousewheel.min.js';
 */
+
+import './assets/vendor/jquery-localize/dist/jquery.localize.min.js';
 
 import './assets/vendor/matchMedia/matchMedia.js';
 import './assets/vendor/matchMedia/matchMedia.addListener.js';
