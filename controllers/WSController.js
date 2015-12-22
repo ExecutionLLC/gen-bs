48f64e21d018590f907d41ff61f58b39cfeb789a
Vasily Loginov <loginov.vasily@gmail.com>
2015-12-22 17:47:58 +0600

    Working on WebSocket server.

--- controllers/WSController.js
+++ controllers/WSController.js
@@ -1,17 +1,37 @@
 'use strict';
 
 const Express = require('express');
+const Http = require('http');
+const WebSocketServer = require('ws').Server;
 
 const ControllerBase = require('./ControllerBase');
 
 class WSController extends ControllerBase {
     constructor(services) {
         super(services);
-
     }
 
     createRouter() {
+        // Start web socket server.
+        const httpServer = Http.createServer();
+        const webSocketServer = new WebSocketServer({
+            server: httpServer
+        });
+        webSocketServer.on('connection', (ws) => {
+            ws.on('message', (message) => {
+                console.log('WS: Received ' + message);
+                ws.send(message);
+            });
+        });
+
         const router = new Express();
+
+        router.ws('/echo', (ws, request) => {
+            ws.on('message', (message) => {
+                console.log(message);
+                ws.send(message);
+            })
+        });
 
         return router;
     }
