87e888aa85593c31ee117e8696de1c6f6e83ea7a
Sergey Vatagin <s.vatagin@gmail.com>
2015-12-24 20:02:26 +0300

    fix table rendering but without redux yet

--- frontend/app/components/SamplesTable/VariantsTable.js
+++ frontend/app/components/SamplesTable/VariantsTable.js
@@ -4,5 +4,38 @@ import { initializeTable, setColumnFilter } from '../../actions'
 
 import store from '../../containers/App'
 
-const tableElement = document.getElementById('root')
+const $tableElement = $('#variants_table');
+
+$( () => {
+
+  function fillRows(tData) {
+      return tData.map( (rowData) => {
+        var row = [];
+
+        row.push('<tr>');
+        for(var key in rowData) {
+          row.push(`<td>${rowData[key]}</td>`);
+        }
+        row.push('</tr>');
+
+        return row.join();
+      });
+  }
+
+
+  function getInitialState() {
+    $.get(json, function(data) {
+      render(fillRows(data));
+    })
+  }
+
+  function render(tableRows) {
+    $tableElement.append(tableRows)
+  }
+
+  getInitialState();
+
+
+
+});
 
