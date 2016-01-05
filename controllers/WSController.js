44ebba7b60ad62283f6fef53ad80c03d00d94bf3
Vasily Loginov <loginov.vasily@gmail.com>
2016-01-05 14:59:15 +0600

    Started implementation of the client WS interaction.

--- controllers/WSController.js
+++ controllers/WSController.js
@@ -3,23 +3,31 @@
 const Express = require('express');
 
 const ControllerBase = require('./ControllerBase');
+const ChangeCaseUtil = require('../utils/ChangeCaseUtil');
 
 class WSController extends ControllerBase {
     constructor(services) {
         super(services);
+
+        this.clientsBySession = {};
     }
 
     addWebSocketServerCallbacks(webSocketServer) {
         webSocketServer.on('connection', (ws) => {
             ws.on('message', (message) => {
-                console.log('Received: ' + message);
-                ws.send(message, (error) => {
-                    if (error) {
-                        console.error('Client WS send error', error);
-                    }
-                });
+                const convertedMessage = ChangeCaseUtil.convertKeysToCamelCase(message);
+                console.log('Received: ' + JSON.stringify(message, null, 2));
+                this.onClientMessage(ws, convertedMessage);
             });
         });
+    }
+
+    onClientMessage(clientWs, message) {
+        const sessionId = message.sessionId;
+        if (sessionId) {
+            console.log('Connecting client WS to session ' + sessionId);
+            this.clientsBySession[sessionId] = clientWs;
+        }
     }
 }
 
--- services/WSService.js
+++ services/WSService.js
@@ -4,10 +4,38 @@ const _ = require('lodash');
 
 const ServiceBase = require('./ServiceBase');
 
+const EventEmitter = require('events').EventEmitter;
+
+const EVENTS = {
+    operationProgress: 'operationProgress'
+};
+
 class WSService extends ServiceBase {
     constructor(services) {
         super(services);
+
+        this.eventEmitter = new EventEmitter();
+        this._registerAppServerEvents();
     }
+
+    registeredEvents() {
+        return EVENTS;
+    }
+
+    on(eventName, callback) {
+        this.eventEmitter.on(eventName, callback);
+    }
+
+    off(eventName, callback) {
+        this.eventEmitter.removeListener(eventName, callback);
+    }
+
+    _registerAppServerEvents() {
+        const appServer = this.services.applicationServer;
+        const AppServerEvents = appServer.registeredEvents();
+
+    }
+
 }
 
 module.exports = WSService;
