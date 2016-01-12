5004997d20da7650ac9f533ba4c464f720652f22
bormaxi <maxim.borschev@execution.su>
2016-01-12 16:59:40 +0600

    KnexTransaction.id parameter added

--- utils/KnexTransaction.js
+++ utils/KnexTransaction.js
@@ -1,14 +1,18 @@
 'use strict';
 
+const Uuid = require('node-uuid');
+
 class KnexTransaction {
     constructor(knex, logger) {
         this.knex = knex;
         this.logger = logger;
         this.transaction = null;
+        this.id = Uuid.v4();
     };
 
     openTransaction(callback) {
         this.knex.transaction((trx) => {
+            this.logger.info('OPENING TRANSACTION ' + this.id);
             this.transaction = trx;
             callback(null, trx);
         });
@@ -16,14 +20,14 @@ class KnexTransaction {
 
     complete(error, data, callback) {
         if (error) {
-            this.logger.warn("ROLLING BACK TRANSACTION: " + error);
+            this.logger.warn('ROLLING BACK TRANSACTION ' + this.id +': ' + error);
             this.transaction
                 .rollback()
                 .asCallback(error => {
                     callback(error);
                 });
         } else {
-            this.logger.info("COMMITING TRANSACTION");
+            this.logger.info('COMMITING TRANSACTION ' + this.id);
             this.transaction
                 .commit()
                 .asCallback((error) => {
