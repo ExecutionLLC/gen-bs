20363d0e022db75053cca826236e16d11c0cbe1c
Sergey Vatagin <s.vatagin@gmail.com>
2016-01-13 01:35:04 +0300

    add TODO remark to views action

--- frontend/app/actions/views.js
+++ frontend/app/actions/views.js
@@ -4,6 +4,7 @@ export const CHANGE_VIEW = 'CHANGE_VIEW'
  * Action Creators
  */
 
+// TODO: move views list from here or from user_data
 export function changeView(views, selectedViewId) {
   return {
     type: CHANGE_VIEW,
