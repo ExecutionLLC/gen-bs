67c74c47db4ca009a4a094b46e48b55752d68207
Vasily Loginov <loginov.vasily@gmail.com>
2016-01-14 19:18:41 +0600

    Added PS1.

--- scripts/amazon-install.sh
+++ scripts/amazon-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Script is used to configure Amazon instance
 
+PS1='$ '
+
 echo "\n=> Install Node Version Manager..."
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
 
