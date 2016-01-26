57bde17c9139b211b5b04d1d23257c5d7379afa3
bormaxi <maxim.borschev@execution.su>
2016-01-26 16:44:59 +0600

    Temporary commit

--- utils/Config.js
+++ utils/Config.js
@@ -6,14 +6,14 @@ const SETTINGS = {
     port: ENV.GEN_WS_PORT || 5000,
     sessionHeader: ENV.GEN_WS_SESSION_HEADER || 'X-Session-Id',
     applicationServer: {
-        host: ENV.GEN_WS_AS_HOST || 'localhost',
+        host: ENV.GEN_WS_AS_HOST || '192.168.0.1',
         port: ENV.GEN_WS_AS_PORT || 8000
     },
     database: {
         host: ENV.GEN_WS_DATABASE_SERVER || 'localhost',
         port: ENV.GEN_WS_DATABASE_PORT | 5432,
         user: ENV.GEN_WS_DATABASE_USER || 'postgres',
-        password: ENV.GEN_WS_DATABASE_PASSWORD || 'zxcasdqwe',
+        password: ENV.GEN_WS_DATABASE_PASSWORD || 'postgres',
         databaseName: ENV.GEN_WS_DATABASE_NAME || 'genomixdb'
     },
     logger: {
