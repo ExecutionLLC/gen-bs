f553b4b337c6d62dd1f73b121d67cc27f40336c2
bormaxi <maxim.borschev@execution.su>
2016-02-09 09:35:00 +0600

    View list items check added

--- services/ViewsService.js
+++ services/ViewsService.js
@@ -11,27 +11,44 @@ class ViewsService extends UserEntityServiceBase {
   }
 
   add(user, languId, item, callback) {
-    if (!_.isArray(item.viewListItems)) {
-        callback(new Error('View list items must be an array'));
-    } else if (item.viewListItems.length === 0) {
-        callback(new Error('View list items must contain at least one element'));
-    } else {
-        super.add(user, languId, item, callback);
-    }
+    async.waterfall([
+        (callback) => {
+            this._viewItemsCheck(item, callback);
+        },
+        (item, callback) => {
+            super.add(user, languId, item, callback);
+        }
+    ], callback);
   }
 
   update(user, view, callback) {
       async.waterfall([
           (callback) => super.find(user, view.id, callback),
           (existingView, callback) => {
-          if (existingView.type !== 'user') {
-              callback(new Error('Default view cannot be updated'));
-          } else {
-              super.update(user, view, callback);
+              this._viewItemsCheck(existingView, callback);
+          },
+          (existingView, callback) => {
+              if (existingView.type !== 'user') {
+                  callback(new Error('Default view cannot be updated'));
+              } else {
+                super.update(user, view, callback);
+              }
           }
-        }
       ], callback);
   }
+
+  _viewItemsCheck(view, callback) {
+      if (!view.viewListItems) {
+          callback(new Error('View list items must be defined'))
+      } else if (!_.isArray(view.viewListItems)) {
+          callback(new Error('View list items must be an array'));
+      } else if (item.viewListItems.length === 0) {
+          callback(new Error('View list items must contain at least one element'));
+      } else {
+        callback(null, view);
+      }
+  }
+
 }
 
 module.exports = ViewsService;
