c97878358580f5ac818600a4a41893307b51619b
Sergey Vatagin <s.vatagin@gmail.com>
2016-02-17 13:19:55 +0300

    make gzip helper working. Input - File. Output - gzipped Blob with .gz addition to filename

--- frontend/app/utils/gzip.js
+++ frontend/app/utils/gzip.js
@@ -1,4 +1,22 @@
+/**
+ * gzip file
+ * @param {string} file - File for gzip.
+ */
 export default function gzip(file) {
   const zip = new JSZip;
+  const reader = new FileReader();
+  var result = null;
 
+  reader.onload = (function(e) {
+      var blob = zip.generate({type:"blob"});
+      var ch = new Uint8Array(e.target.result);
+      zip.file('hello111.txt', ch);
+      var content = zip.generate({type:"blob"});
+      content.lastModifiedDate = new Date();
+      content.name = file.name + '.gz'
+      window.result = content
+  });
+
+  reader.readAsArrayBuffer(file)
 }
+
