7e8a0ed94730b1f5f5b8425ca1a0f62235c84b8d
Sergey Vatagin <s.vatagin@gmail.com>
2016-02-29 16:24:26 +0300

    add google auth link to header`s Login(Register)

--- frontend/app/components/Header/NavbarMain/Auth.js
+++ frontend/app/components/Header/NavbarMain/Auth.js
@@ -11,7 +11,7 @@ export default class Auth extends Component {
 
       <div>
         <div data-toggle="tooltip" data-localize="account.help"  data-placement="left" title="Register or login for access to featured options" data-container="body" data-trigger="hover" className="hidden-xs hidden-xss">
-            <a href="#" className="btn navbar-btn dropdown-toggle" data-toggle="collapse" data-target="#sidebarRight"><span className="guest" data-localize="account.login.title">Login<span className="hidden-xs"> (<span data-localize="account.register.title">Register</span>)</span></span><span className="user hidden"><span>Name</span><span>Lastname</span></span></a>
+          <a href={`http://${API_HOST}:${API_PORT}/api/session/auth/google?callbackPort=8080`} className="btn navbar-btn dropdown-toggle" data-toggle="collapse" data-target="#sidebarRight"><span className="guest" data-localize="account.login.title">Login<span className="hidden-xs"> (<span data-localize="account.register.title">Register</span>)</span></span><span className="user hidden"><span>Name</span><span>Lastname</span></span></a>
         </div>
         <div className="visible-xs visible-xss">
           <a href="#" className="btn navbar-btn dropdown-toggle" data-toggle="collapse" data-target="#sidebarRight"><span className="guest" data-localize="account.login.title">Login</span><span className="user hidden"><span>Name</span><span>Lastname</span></span></a>
