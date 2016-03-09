c707d39886dc8bfc97fd3aa8a6f4e5623e16e728
Deonis Peretyagin <karudo@gmail.com>
2016-03-09 08:26:49 +0600

    serach by file_name

--- frontend/app/components/Modals/FileUpload/FileUploadSamples.js
+++ frontend/app/components/Modals/FileUpload/FileUploadSamples.js
@@ -3,16 +3,24 @@ import React, { Component } from 'react';
 import FileUploadSamplesRow from './FileUploadSamplesRow';
 
 export default class FileUploadSamples extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { searchWord: '' };
+  }
   render() {
+    let { samples } = this.props;
+    if (this.state.searchWord.length > 0) {
+      samples = samples.filter(el => ~el.file_name.indexOf(this.state.searchWord));
+    }
     return (
       <div>
-        <h4 data-localize="samples.search.label">Searh for available samples</h4>
+        <h4 data-localize="samples.search.label">Search for available samples</h4>
         <div className="form-group has-feedback">
-          <input type="text" className="form-control"/>
+          <input type="text" onChange={e => this.setState({ searchWord: e.target.value })} className="form-control"/>
           <span className="form-control-feedback"><i className="fa fa-lg fa-search text-muted"/></span>
         </div>
         <div className="panel-group">
-          {this.props.samples.map(
+          {samples.map(
             sample => <FileUploadSamplesRow sample={sample} fields={this.props.fieldsList} key={sample.id}/>
           )}
         </div>
