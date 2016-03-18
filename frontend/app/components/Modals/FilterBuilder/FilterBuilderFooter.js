3e6a4ccb306f31385b3cd8b17c39bd4981580c0b
Vasily Loginov <loginov.vasily@gmail.com>
2016-03-18 16:18:17 +0600

    Fixed formatting.

--- frontend/app/components/Modals/FilterBuilder/FilterBuilderFooter.js
+++ frontend/app/components/Modals/FilterBuilder/FilterBuilderFooter.js
@@ -8,22 +8,19 @@ import { filterBuilderRequestRules } from '../../../actions/filterBuilder';
 export default class FilterBuilderFooter extends Component {
 
   render() {
+    const { dispatch, auth, closeModal } = this.props;
+    const { editOrNew, editedFilter, newFilter } = this.props.filterBuilder;
+    const filter = editOrNew ? (editedFilter):(newFilter);
 
-    const { dispatch, auth, closeModal } = this.props
-    const { editOrNew, editedFilter, newFilter } = this.props.filterBuilder
-    const filter = editOrNew ? (editedFilter):(newFilter)
-
-    var disabledClass = classNames({
+    const disabledClass = classNames({
       'disabled': (filter.type === 'advanced' && auth.isDemo) ? 'disabled':''
-    })
-      var title = (filter.type === 'advanced' && auth.isDemo) ? 'Login or registry to select advanced filters':''
-
+    });
+    const title = (filter.type === 'advanced' && auth.isDemo) ? 'Login or register to select advanced filters':'';
 
     return (
-
         <Modal.Footer>
           <button
-            onClick={ () => { this.props.closeModal('filters')} }
+            onClick={ () => { closeModal('filters')} }
             type="button"
             className="btn btn-default"
             data-dismiss="modal"
