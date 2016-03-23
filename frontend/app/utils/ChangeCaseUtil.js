c645bbca8d7ba1cbf571fe991a35928f8f206a40
MadHatter <ivkalgin@gmail.com>
2016-03-23 12:32:00 +0600

    fixed code style

--- frontend/app/utils/ChangeCaseUtil.js
+++ frontend/app/utils/ChangeCaseUtil.js
@@ -4,23 +4,22 @@ const changeCase = require('change-case');
 const _ = require('lodash');
 
 export default class ChangeCaseUtil {
-  static convertKeysToCamelCase(obj) {
-      return ChangeCaseUtil._processObjectKeys(obj, changeCase.camelCase);
-  }
+    static convertKeysToCamelCase(obj) {
+        return ChangeCaseUtil._processObjectKeys(obj, changeCase.camelCase);
+    }
 
-  static convertKeysToSnakeCase(obj) {
-      return ChangeCaseUtil._processObjectKeys(obj, changeCase.snakeCase);
-  }
+    static convertKeysToSnakeCase(obj) {
+        return ChangeCaseUtil._processObjectKeys(obj, changeCase.snakeCase);
+    }
 
-  static _processObjectKeys(obj, mutatorFunc) {
-      if (!obj) {
-          return obj;
-      }
+    static _processObjectKeys(obj, mutatorFunc) {
+        if (!obj) {
+            return obj;
+        }
 
-      if (_.isObject(obj)) {
-          const clone = _.cloneDeep(obj);
-          _.keys(clone)
-            .forEach(key => {
+        if (_.isObject(obj)) {
+            const clone = _.cloneDeep(obj);
+            _.keys(clone).forEach(key => {
                 if (_.every(key, ChangeCaseUtil._isAlphanumericOrDash)) {
                     const mutatedKey = mutatorFunc(key);
                     const value = clone[key];
@@ -32,21 +31,21 @@ export default class ChangeCaseUtil {
                     clone[key] = ChangeCaseUtil._processObjectKeys(value, mutatorFunc);
                 }
             });
-          return clone;
-      } else if (_.isArray(obj)) {
-          const arr = obj;
-          return _.map(arr, item => ChangeCaseUtil._processObjectKeys(item, mutatorFunc));
-      } else {
-          return obj;
-      }
-  }
+            return clone;
+        } else if (_.isArray(obj)) {
+            const arr = obj;
+            return _.map(arr, item => ChangeCaseUtil._processObjectKeys(item, mutatorFunc));
+        } else {
+            return obj;
+        }
+    }
 
-  static _isAlphanumericOrDash(char) {
-    const code = (char) => char.charCodeAt(0);
-    const charCode = code(char);
-    return char === '_'
-        || (charCode >= code('a') && charCode <= code('z'))
-        || (charCode >= code('A') && charCode <= code('Z'))
-        || (charCode >= code('0') && charCode <= code('9'));
-  }
+    static _isAlphanumericOrDash(char) {
+        const code = (char) => char.charCodeAt(0);
+        const charCode = code(char);
+        return char === '_'
+            || (charCode >= code('a') && charCode <= code('z'))
+            || (charCode >= code('A') && charCode <= code('Z'))
+            || (charCode >= code('0') && charCode <= code('9'));
+    }
 }
