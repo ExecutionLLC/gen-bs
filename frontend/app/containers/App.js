047f2ee4fa8f5e70529f3ca5b979091c48a39bfc
MadHatter <ivkalgin@gmail.com>
2016-03-24 18:28:37 +0600

    error dialog integrated to application

--- frontend/app/containers/App.js
+++ frontend/app/containers/App.js
@@ -9,9 +9,11 @@ import NavbarCreateQuery from '../components/Header/NavbarCreateQuery'
 import ViewsModal from '../components/Modals/ViewsModal'
 import FiltersModal from '../components/Modals/FiltersModal'
 import FileUploadModal from '../components/Modals/FileUploadModal'
+import ErrorModal from '../components/Modals/ErrorModal'
 
 import { login } from '../actions/auth'
 import { openModal, closeModal } from '../actions/modalWindows'
+import { lastErrorResolved } from '../actions/errorHandler'
 import { fetchUserdata } from '../actions/userData'
 
 
@@ -33,61 +35,53 @@ class App extends Component {
         });
 
         return (
-
-      <div className={mainDivClass} id="main">
-        <nav className="navbar navbar-inverse navbar-static-top"></nav>
-        {!isAuthenticated &&
-          <div >&nbsp;</div>
-        }
-        {isAuthenticated && isFetching && samples.length === 0 &&
-          <div className="loader"><h1>Analyze...</h1></div>
-        }
-        {isAuthenticated && !isFetching && samples.length === 0 &&
-          <h2>Empty.</h2>
-        }
-        {samples.length > 0 &&
-            <div className="container-fluid">
-
-
-
-                        <NavbarMain />
-                        <div className="collapse collapse-subnav" id="subnav">
-                            <NavbarCreateQuery
-                              {...this.props}
-                              openModal={ (modalName) => { this.props.dispatch(openModal(modalName)) } }
-                            />
-                        </div>
-                        <VariantsTableReact {...this.props} />
-
-
-                    <div id="fav-message" className="hidden">
+            <div className={mainDivClass} id="main">
+                <nav className="navbar navbar-inverse navbar-static-top"></nav>
+                {!isAuthenticated && <div >&nbsp;</div>}
+                {isAuthenticated && isFetching && samples.length === 0 &&
+                    <div className="loader"><h1>Analyze...</h1></div>
+                }
+                {isAuthenticated && !isFetching && samples.length === 0 &&
+                    <h2>Empty.</h2>
+                }
+                {samples.length > 0 &&
+                 <div className="container-fluid">
+                    <NavbarMain />
+                     <div className="collapse collapse-subnav" id="subnav">
+                         <NavbarCreateQuery
+                          {...this.props}
+                          openModal={ (modalName) => { this.props.dispatch(openModal(modalName)) } }
+                         />
+                     </div>
+                     <VariantsTableReact {...this.props} />
+                     <div id="fav-message" className="hidden">
                         You can export these items to file
-                    </div>
-
-                    </div>
-
-        }
-
-        <ViewsModal
-          showModal={this.props.modalWindows.views.showModal}
-          closeModal={ (modalName) => { this.props.dispatch(closeModal(modalName)) } }
-        />
-        <FiltersModal
-          showModal={this.props.modalWindows.filters.showModal}
-          closeModal={ (modalName) => { this.props.dispatch(closeModal(modalName)) } }
-        />
-        <FileUploadModal
-          showModal={this.props.modalWindows.upload.showModal}
-          closeModal={ (modalName) => { this.props.dispatch(closeModal(modalName)) } }
-        />
-
-        </div>
-    )
-  }
+                     </div>
+                 </div>
+                }
+                <ErrorModal
+                    showModal={this.props.showErrorWindow}
+                    closeModal={ () => { this.props.dispatch(lastErrorResolved()) } }
+                />
+                <ViewsModal
+                    showModal={this.props.modalWindows.views.showModal}
+                    closeModal={ (modalName) => { this.props.dispatch(closeModal(modalName)) } }
+                />
+                <FiltersModal
+                    showModal={this.props.modalWindows.filters.showModal}
+                    closeModal={ (modalName) => { this.props.dispatch(closeModal(modalName)) } }
+                />
+                <FileUploadModal
+                    showModal={this.props.modalWindows.upload.showModal}
+                    closeModal={ (modalName) => { this.props.dispatch(closeModal(modalName)) } }
+                />
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state) {
-    const { auth, userData, modalWindows, views, fields, ui } = state
+    const { auth, userData, modalWindows, views, fields, ui, errorHandler: { showErrorWindow } } = state
 
     return {
         auth,
@@ -95,9 +89,9 @@ function mapStateToProps(state) {
         modalWindows,
         views,
         fields,
-        ui
+        ui,
+        showErrorWindow
     }
 }
 
 export default connect(mapStateToProps)(App)
-
