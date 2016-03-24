27d4e7cb802c2e1f1de7fc210906f1b025cfec27
Vasily Loginov <loginov.vasily@gmail.com>
2016-03-24 12:17:04 +0600

    Added switch for performance buttons.

--- frontend/app/components/Header/NavbarMain/CreateQueryNavbarButton.js
+++ frontend/app/components/Header/NavbarMain/CreateQueryNavbarButton.js
@@ -4,6 +4,11 @@ import Perf from 'react-addons-perf';
 
 
 export default class CreateQeuryNavbarButton extends Component {
+    constructor(props) {
+        super(props);
+
+        this.enablePerformanceControls = false;
+    }
 
     onStop() {
         Perf.stop();
@@ -22,10 +27,14 @@ export default class CreateQeuryNavbarButton extends Component {
          <div className="visible-xs"><a type="button" href="#" className="btn navbar-btn" data-toggle="modal" data-target="#analysis"><i className="md-i">settings</i></a></div>
          
          <div className="hidden-xs"  data-localize="query.help" data-toggle="tooltip" data-placement="right" title="Open navbar and create new analises query" data-container="body" data-trigger="hover">
-           <a onClick={this.props.toggleQueryNavbar} type="button" href="#" className="btn navbar-btn" id="btnToggle" data-target="#subnav" data-toggle="collapse">Anal
+           <a onClick={this.props.toggleQueryNavbar} type="button" href="#" className="btn navbar-btn" id="btnToggle" data-target="#subnav" data-toggle="collapse">Analyze
          </a>
-             <button onClick={() => Perf.start()}>B</button>
-             <button onClick={() => this.onStop()}>E</button>
+             {this.enablePerformanceControls &&
+                 <div>
+                     <button onClick={() => Perf.start()}>B</button>
+                     <button onClick={() => this.onStop()}>E</button>
+                 </div>
+             }
          </div>
       </div>
 
