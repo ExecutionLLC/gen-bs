b64d1b41e6b63255a6ba0dcaec387ec930c7955e
MadHatter <ivkalgin@gmail.com>
2016-03-24 18:26:47 +0600

    fixed code style

--- frontend/app/components/Modals/FilterBuilder/FilterBuilderHeader.js
+++ frontend/app/components/Modals/FilterBuilder/FilterBuilderHeader.js
@@ -3,16 +3,13 @@ import { Modal } from 'react-bootstrap';
 
 
 export default class FilterBuilderHeader extends Component {
-
-  render() {
-    return (
-
-          <Modal.Header closeButton>
-            <Modal.Title data-localize="filters.heading">
-              Setup filters
-            </Modal.Title>
-          </Modal.Header>
-
-    )
-  }
+    render() {
+        return (
+            <Modal.Header closeButton>
+                <Modal.Title data-localize="filters.heading">
+                    Setup filters
+                </Modal.Title>
+            </Modal.Header>
+        )
+    }
 }
