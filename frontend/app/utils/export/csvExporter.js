d39ef2a925eedd1addcf38608458436d3be537ea
Vasily Loginov <loginov.vasily@gmail.com>
2016-03-28 20:33:53 +0600

    Added CSV export realization.

---
+++ frontend/app/utils/export/csvExporter.js
@@ -0,0 +1,28 @@
+export default class CsvExporter {
+    constructor() {
+        this.separator = ',';
+        this.newLine = '\n';
+        this.mimeType = '"application/csv"';
+    }
+
+    /**
+     * Creates new Blob containing 
+     * @param columnsArray Array of objects, each has {id, name}
+     * @param data Array of objects, each has {columnId, value}
+     * */
+    build(columnsArray, data) {
+        const columns = _.map(columnsArray, col => col.name);
+        const headerRow = this._createRow(columns);
+        const rows = _.map(data, row => {
+            const orderedValues = _.map(columnsArray, column => row[column.id] || null);
+            return this._createRow(orderedValues);
+        });
+
+        const documentBody = headerRow + this.newLine + rows.join(this.newLine);
+        return new Blob([documentBody], {type: this.mimeType});
+    }
+
+    _createRow(rowValues) {
+        return rowValues.join(this.separator);
+    }
+}
