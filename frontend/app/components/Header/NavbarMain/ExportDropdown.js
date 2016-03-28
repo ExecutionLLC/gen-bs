16551052e08c53c832017918d7d7a60891dff930
Vasily Loginov <loginov.vasily@gmail.com>
2016-03-28 20:57:37 +0600

    Implemented CSV export button stub.

--- frontend/app/components/Header/NavbarMain/ExportDropdown.js
+++ frontend/app/components/Header/NavbarMain/ExportDropdown.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {Nav, NavDropdown, MenuItem} from 'react-bootstrap';
+
+import ExportUtils from '../../../utils/exportUtils';
 
 export default class ExportDropdown extends Component {
 
@@ -9,21 +12,66 @@ export default class ExportDropdown extends Component {
     render() {
         return (
             <div>
-                <div data-localize="files.export.help"
-                     data-toggle="tooltip"
-                     data-placement="right"
-                     title="Select several mutations in the table and save to file. There are indicate the number of already selected recordings"
-                     data-container="body"
-                     data-trigger="hover">
+                <Nav>
+                    <NavDropdown title="Export"
+                                 id="export-dropdown"
+                                 onSelect={(e, item) => this.onExportItemSelected(e, item)}
 
-                    <a className="btn navbar-btn" type="button" id="dropdownMenu1" data-toggle="modal"
-                       data-target="#filename">
-                        <span className="hidden-xxs" data-localize="files.export.title">Export</span>
-                        <span className="visible-xxs"><i className="md-i">file_download</i></span>
-                        <span className="badge badge-warning">7</span>
-                    </a>
-                </div>
+                    >
+                        <MenuItem eventKey="csv">CSV</MenuItem>
+                        <MenuItem eventKey="sql">SQL</MenuItem>
+                        <MenuItem eventKey="txt">Text</MenuItem>
+                    </NavDropdown>
+                </Nav>
+                {
+                    // <div data-localize="files.export.help"
+                    //      data-toggle="tooltip"
+                    //      data-placement="right"
+                    //      title="Select several mutations in the table and save to file. There are indicate the number of already selected recordings"
+                    //      data-container="body"
+                    //      data-trigger="hover">
+                    //
+                    //     <a className="btn navbar-btn" type="button" id="dropdownMenu1" data-toggle="modal"
+                    //        data-target="#filename">
+                    //         <span className="hidden-xxs" data-localize="files.export.title">Export</span>
+                    //         <span className="visible-xxs"><i className="md-i">file_download</i></span>
+                    //         <span className="badge badge-warning">7</span>
+                    //     </a>
+                    // </div>
+                }
             </div>
         );
+    }
+
+    onExportItemSelected(event, selectedKey) {
+        event.preventDefault();
+
+        const exporter = ExportUtils.createExporter(selectedKey);
+        if (!exporter) {
+            console.error('No exporter of type "' + ofType + '" is found');
+            return;
+        }
+        const columns = [
+            {
+                id: 1,
+                name: 'Column1'
+            },
+            {
+                id: 2,
+                name: 'Column2'
+            }
+        ];
+        const data = [
+            {
+                '1': 'asd',
+                '2': 'qwe'
+            }, {
+                '1': 'qwea',
+                '2': 'aaaa'
+            }
+        ];
+
+        const blob = exporter.build(columns, data);
+        ExportUtils.downloadBlob(blob);
     }
 }
---
+++ frontend/app/utils/exportUtils.js
@@ -0,0 +1,24 @@
+import CsvExporter from './export/csvExporter';
+
+export default class ExportUtils {
+    static createExporter(ofType) {
+        switch (ofType) {
+            case 'csv': {
+                return new CsvExporter();
+            }
+            default:
+                return null;
+        }
+    }
+
+    static downloadBlob(blob, fileName) {
+        const url = window.URL.createObjectURL(blob);
+        var a = document.createElement('a');
+        a.style = 'display: none';
+        document.body.appendChild(a);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+}
