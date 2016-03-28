9454a9ef9b7d9eb6869b005c330274eb4d100d3b
Vasily Loginov <loginov.vasily@gmail.com>
2016-03-28 12:30:54 +0600

    Added semicolon.

--- frontend/app/containers/Root.js
+++ frontend/app/containers/Root.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import configureStore from '../store/configureStore'
 import App from './App'
 
-const store = configureStore()
+const store = configureStore();
 
 export default class Root extends Component {
     render() {
