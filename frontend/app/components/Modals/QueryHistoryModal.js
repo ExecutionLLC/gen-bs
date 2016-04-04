d7bdc335672f59a1913218f47b6705a74230f386
MadHatter <ivkalgin@gmail.com>
2016-04-04 14:30:13 +0600

    fixed rendering of the filter name

--- frontend/app/components/Modals/QueryHistoryModal.js
+++ frontend/app/components/Modals/QueryHistoryModal.js
@@ -67,9 +67,9 @@ export default class QueryHistoryModal extends Component {
 
     renderHistoryTableRow(historyItem) {
         const itemId = historyItem.id;
-        const datetime = historyItem.timestamp ? historyItem.timestamp.toString() : 'Unknown';
-        const sample = historyItem.sample ? historyItem.sample.name : 'Unknown';
-        const filters = historyItem.filters ? historyItem.filters : 'Unknown';
+        const datetime = historyItem.timestamp ? (new Date(historyItem.timestamp)).toString() : 'Unknown';
+        const sample = historyItem.sample ? historyItem.sample.fileName : 'Unknown';
+        const filters = historyItem.filters.length > 0 ? _.map(historyItem.filters, (item) => { return item.name }).join('</br>') : 'Unknown';
         const view = historyItem.view ? historyItem.view.name : 'Unknown';
         return (
             <tr key={ itemId }>
