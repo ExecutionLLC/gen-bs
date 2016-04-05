274f0a1f3a8bc6dbda7f048021c5b338237cec39
MadHatter <ivkalgin@gmail.com>
2016-04-05 15:39:37 +0600

    fixed imports of session client

--- frontend/app/api/SamplesClient.js
+++ frontend/app/api/SamplesClient.js
@@ -1,10 +1,9 @@
 'use strict';
 
-const assert = require('assert');
-const _ = require('lodash');
+import _ from 'lodash';
 
-const RequestWrapper = require('./RequestWrapper');
-const UserEntityClientBase = require('./UserEntityClientBase');
+import RequestWrapper from './RequestWrapper';
+import UserEntityClientBase from './UserEntityClientBase';
 
 export default class SamplesClient extends UserEntityClientBase {
     constructor(urls) {
@@ -67,7 +66,7 @@ export default class SamplesClient extends UserEntityClientBase {
         if (sampleOrNull) {
             const values = sampleOrNull.values;
             var ok = _.each(values, value => {
-                !_.any(fieldsMetadata, fieldMetadata => fieldMetadata.id === value.fieldId));
+                !_.any(fieldsMetadata, fieldMetadata => fieldMetadata.id === value.fieldId);
             });
             return ok;
         }
