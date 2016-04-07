c24f708625590f67b4bf5465fa7ed3b161edf4af
Vasily Loginov <loginov.vasily@gmail.com>
2016-04-07 20:52:31 +0600

    Reformatted file.

--- models/QueryHistoryModel.js
+++ models/QueryHistoryModel.js
@@ -40,55 +40,54 @@ class QueryHistoryModel extends SecureModelBase {
 
     findAll(userId, limit, offset, callback) {
         async.waterfall([
-                (callback) => this._findQueryHistoryIds(userId, limit, offset, callback),
-                (queryHistoryIds, callback) => this._findQueryHistoryByIds(userId, queryHistoryIds, callback),
-                (queryHistoryItems, callback) => {
-                    const viewIds = _.map(queryHistoryItems,
-                        (queryHistoryItem) => queryHistoryItem.viewId
-                    );
-                    const sampleIds = _.map(queryHistoryItems,
-                        (queryHistoryItem) => queryHistoryItem.sampleId
-                    );
-                    const filterIds = _(queryHistoryItems)
-                        .map(queryHistoryItem => queryHistoryItem.filterIds)
-                        .flatten()
-                        .value();
+            (callback) => this._findQueryHistoryIds(userId, limit, offset, callback),
+            (queryHistoryIds, callback) => this._findQueryHistoryByIds(userId, queryHistoryIds, callback),
+            (queryHistoryItems, callback) => {
+                const viewIds = _.map(queryHistoryItems,
+                    (queryHistoryItem) => queryHistoryItem.viewId
+                );
+                const sampleIds = _.map(queryHistoryItems,
+                    (queryHistoryItem) => queryHistoryItem.sampleId
+                );
+                const filterIds = _(queryHistoryItems)
+                    .map(queryHistoryItem => queryHistoryItem.filterIds)
+                    .flatten()
+                    .value();
 
-                    async.parallel({
-                        samples: (callback) => {
-                            this.models.samples.findMany(userId, _.uniq(sampleIds), callback);
-                        },
-                        views: (callback) => {
-                            this.models.views.findMany(userId, _.uniq(viewIds), callback);
-                        },
-                        filters: (callback) => {
-                            this.models.filters.findMany(userId, _.uniq(filterIds), callback);
-                        }
-                    }, (error, result)=> {
-                        callback(error, result.samples, result.views, result.filters, queryHistoryItems);
-                    });
-                },
-                (samples, views, filters, queryHistory, callback) => {
-                    // Create hashes.
-                    const samplesHash = CollectionUtils.createHashByKey(samples, 'id');
-                    const filtersHash = CollectionUtils.createHashByKey(filters, 'id');
-                    const viewsHash = CollectionUtils.createHashByKey(views, 'id');
-                    // Create resulting object.
-                    const resultQueryHistory = _.map(queryHistory, query => {
-                        return {
-                            id: query.id,
-                            timestamp: query.timestamp,
-                            view: viewsHash[query.viewId],
-                            filters: _.map(query.filterIds, filterId => filtersHash[filterId]),
-                            sample: samplesHash[query.sampleId]
-                        };
-                    });
-                    callback(null, resultQueryHistory);
-                }
-            ], (error, historyItems) =>  {
-                callback(error, historyItems);
+                async.parallel({
+                    samples: (callback) => {
+                        this.models.samples.findMany(userId, _.uniq(sampleIds), callback);
+                    },
+                    views: (callback) => {
+                        this.models.views.findMany(userId, _.uniq(viewIds), callback);
+                    },
+                    filters: (callback) => {
+                        this.models.filters.findMany(userId, _.uniq(filterIds), callback);
+                    }
+                }, (error, result)=> {
+                    callback(error, result.samples, result.views, result.filters, queryHistoryItems);
+                });
+            },
+            (samples, views, filters, queryHistory, callback) => {
+                // Create hashes.
+                const samplesHash = CollectionUtils.createHashByKey(samples, 'id');
+                const filtersHash = CollectionUtils.createHashByKey(filters, 'id');
+                const viewsHash = CollectionUtils.createHashByKey(views, 'id');
+                // Create resulting object.
+                const resultQueryHistory = _.map(queryHistory, query => {
+                    return {
+                        id: query.id,
+                        timestamp: query.timestamp,
+                        view: viewsHash[query.viewId],
+                        filters: _.map(query.filterIds, filterId => filtersHash[filterId]),
+                        sample: samplesHash[query.sampleId]
+                    };
+                });
+                callback(null, resultQueryHistory);
             }
-        );
+        ], (error, historyItems) => {
+            callback(error, historyItems);
+        });
     }
 
     getLastInsertedId(userId, callback) {
@@ -190,7 +189,9 @@ class QueryHistoryModel extends SecureModelBase {
 
         const id = camelcaseRawQueryHistory[0].id;
         const filterIds = _.map(camelcaseRawQueryHistory,
-            (rawQueryHistoryItem) => { return rawQueryHistoryItem.filterId; }
+            (rawQueryHistoryItem) => {
+                return rawQueryHistoryItem.filterId;
+            }
         );
         const sampleId = camelcaseRawQueryHistory[0].vcfFileSampleVersionId;
         const timestamp = camelcaseRawQueryHistory[0].timestamp;
