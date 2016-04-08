4a8fdb6352bd9e8a67f7dd4fb8b25072a032ce26
ponomarevvl90 <ponomarev.vl90@gmail.com>
2016-04-08 13:34:45 +0600

    fix combine filters button

--- frontend/app/components/Header/NavbarCreateQuery/FiltersSetup.js
+++ frontend/app/components/Header/NavbarCreateQuery/FiltersSetup.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Select2 from 'react-select2-wrapper';
 
 
 export default class FiltersSetup extends Component {
 
-  constructor(props) {
-    super(props)
-  }
+    constructor(props) {
+        super(props)
+    }
 
-  render() {
-    return (
+    render() {
+        return (
 
-      <div className="table-cell">
-          <div className="dropdown btn-group" data-localize="filters.setup.help"  data-toggle="tooltip" data-placement="right" data-container="body" title="Setup, combine, create custom new filters or combine default">
-              <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                <span data-localize="filters.title">Filters</span> <span className="caret"></span>
-              </button>
-            <ul className="dropdown-menu dropdown-menu-item-template">
-              <li><a onClick={this.props.openModal.bind(this, 'filters')} href="#" type="button" data-toggle="modal" data-target="#filter" ><span data-localize="filters.setup.title">Setup Filters</span></a></li>
-             
-              <li><a href="#" type="button" data-toggle="modal" data-target="#combinefilter"><span data-localize="filters.combine.title">Combine Filters</span></a></li>
-              
-            </ul>
-          </div>
-     </div>
+            <div className="table-cell">
+                <div className="btn-group" data-localize="filters.setup.help" data-toggle="tooltip"
+                     data-placement="bottom" data-container="body"
+                     title="Setup, create custom new filters">
+                    <button
+                        className="btn btn-default"
+                        type="button"
+                        data-toggle="modal"
+                        data-target="#filter"
+                        onClick={this.props.openModal.bind(this, 'filters')}
+                    >
+                        <span data-localize="filters.setup.title">Filters</span>
+                    </button>
+                </div>
+            </div>
 
 
-    )
-  }
+        )
+    }
 }
