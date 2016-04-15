96f93648a92a54d67ad4c05f27c51160f7cff37c
dmitry <dmitry.zyryanov@execution.su>
2016-04-15 15:20:21 +0600

    Filters refactor PR comments: "InputResizingArray" component extracted

--- frontend/app/components/Modals/FilterBuilder/FilterBuilder.js
+++ frontend/app/components/Modals/FilterBuilder/FilterBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
 import Input from '../../shared/Input';
+import InputResizingArray from '../../shared/InputResizingArray';
 
 import { filterBuilderChangeAll } from '../../../actions/filterBuilder';
 
@@ -741,79 +742,6 @@ class RulesGroupBody extends Component {
  * Input fields arrays, resizeable and fixed sized
  */
 
-const makeKey = (function() {
-    var key;
-    return function() {
-        key = (key + 1) || 0;
-        return key;
-    };
-})();
-
-class InputResizingArray extends Component {
-
-    static toKeyed(vals) {
-        return vals.map( (v) => ({val: v, key: makeKey()}) );
-    }
-
-    static addEmpty(vals) {
-        return vals.concat([{val: '', key: makeKey()}]);
-    }
-
-    static fromKeyed(vals) {
-        return vals.map( (v) => v.val );
-    }
-
-    static removeEmpty(vals) {
-        return vals.filter( (v) => v.val !== '' );
-    }
-
-    static DefaultInput(props) {
-        return (
-            <Input {...props} />
-        );
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: InputResizingArray.addEmpty(InputResizingArray.toKeyed(props.value))
-        };
-    }
-
-    render() {
-        const InputComponent = this.props.InputComponent || InputResizingArray.DefaultInput;
-
-        const self = this;
-
-        function onEditIndex(val, index) {
-            var arr = self.state.value.slice();
-            const isValEmpty = val === '';
-            const isIndexTail = index >= self.state.value.length - 1;
-            arr[index].val = val;
-            if (isValEmpty) {
-            		if (!isIndexTail) {
-                		arr.splice(index, 1);
-                }
-            } else {
-                if (isIndexTail) {
-                    arr = InputResizingArray.addEmpty(arr);
-                }
-            }
-            self.setState({value: arr});
-            self.props.onChange(InputResizingArray.fromKeyed(InputResizingArray.removeEmpty(arr)));
-        }
-
-        return (
-            <div>
-                {this.state.value.map( (val, i) => {
-                    return (
-                        <InputComponent key={val.key} {...this.props} value={val.val} onChange={ (val) => onEditIndex(val, i) } />
-                    );
-                })}
-            </div>
-        );
-    }
-}
 
 class InputArray extends Component {
 
