12a30f5ab1b333f0adad670082ddd2ff780e089e
dmitry <dmitry.zyryanov@execution.su>
2016-04-18 20:14:02 +0600

    filterBuilder refactor: make available filter fields list extracted

--- frontend/app/components/Modals/FilterBuilder/FilterBuilder.js
+++ frontend/app/components/Modals/FilterBuilder/FilterBuilder.js
@@ -4,6 +4,7 @@ import Input from '../../shared/Input';
 import InputResizingArray from '../../shared/InputResizingArray';
 import InputArray from '../../shared/InputArray';
 import QueryBuilder from '../../shared/QueryBuilder';
+import FieldUtils from '../../../utils/fieldUtils'
 
 import { filterBuilderChangeAll } from '../../../actions/filterBuilder';
 
@@ -432,56 +433,12 @@ export default class FilterBuilder extends Component {
             || this.props.filterBuilder !== nextProps.filterBuilder;
     }
 
-    /**
-     * Make field structure usable for filters dialog purpposes
-     * @param {{id: string, label: string, sourceName: string, valueType: string}} f
-     * @param {string=} sourceName
-     * @returns {{id: string, label: string, type: string}}
-     */
-    static getFieldValue(f, sourceName) {
-        return {
-            id: f.id,
-            label: `${f.label} -- ${(sourceName == null ? f.sourceName : sourceName)}`,
-            type: f.valueType === 'float' ? 'double' : f.valueType
-        };
-    }
-
-    /**
-     * Make fields array for filters
-     * @param {{notEditableFields: Object[], sourceFieldsList: Object[]}} fields
-     * @returns {{id: string, label: string, type: string}[]}
-     */
-    static makeFieldsList(fields) {
-
-        /*
-         There was two arrays:
-
-         at componentDidMount:
-         ...fields.notEditableFields.map( (f) => { return {id: f.id, label: `${f.name} -- ${f.sourceName}`, type: f.valueType === 'float' ? 'double' : f.valueType} } ),
-         ...fields.sourceFieldsList.filter((f) => (f.sourceName !== 'sample')).map( (f) => { return {id: f.id, label: `${f.name} -- source`, type: f.valueType === 'float' ? 'double' : f.valueType }} )
-
-         at componentWillUpdate:
-         ...fields.sampleFieldsList.map( (f) => { return {id: f.id, label: `${f.label} -- ${f.sourceName}`, type: f.valueType === 'float' ? 'double' : f.valueType} } ),
-         ...fields.sourceFieldsList.filter((f) => (f.sourceName !== 'sample')).map( (f) => { return {id: f.id, label: `${f.label} -- source`, type: f.valueType === 'float' ? 'double' : f.valueType }} )
-
-         1st part is from 'notEditableFields' at 'componentDidMount' vs 'sampleFieldsList' at 'componentWillUpdate'
-         and all '.name' at componentDidMount vs '.label' at 'componentWillUpdate'
-
-         There must be not editable fields to prevent select gender for the person
-         */
-
-        return [
-            ...fields.notEditableFields.map( (f) => FilterBuilder.getFieldValue(f) ),
-            ...fields.sourceFieldsList.filter( (f) => (f.sourceName !== 'sample') ).map( (f) => FilterBuilder.getFieldValue(f, 'source') )
-        ];
-    }
-
     render() {
         const filter = this.props.filterBuilder.editingFilter.filter;
         return (
             <div className="builder-wrapper">
                 <FilterQueryBuilder
-                    fields={FilterBuilder.makeFieldsList(this.props.fields)}
+                    fields={FieldUtils.makeFieldsList(this.props.fields)}
                     rules={filter.rules}
                     disabled={filter.type === 'standard' || filter.type === 'advanced'}
                     dispatch={this.props.dispatch}
--- frontend/app/utils/fieldUtils.js
+++ frontend/app/utils/fieldUtils.js
@@ -2,4 +2,46 @@ export default class FieldUtils {
     static find(fieldId, fields) {
         return _.find(fields.totalFieldsList, (field) => field.id === fieldId);
     }
+    /**
+     * Make field structure usable for filters dialog purpposes
+     * @param {{id: string, label: string, sourceName: string, valueType: string}} f
+     * @param {string=} sourceName
+     * @returns {{id: string, label: string, type: string}}
+     */
+    static getFieldValue(f, sourceName) {
+        return {
+            id: f.id,
+            label: `${f.label} -- ${(sourceName == null ? f.sourceName : sourceName)}`,
+            type: f.valueType === 'float' ? 'double' : f.valueType
+        };
+    }
+    /**
+     * Make fields array for filters
+     * @param {{notEditableFields: Object[], sourceFieldsList: Object[]}} fields
+     * @returns {{id: string, label: string, type: string}[]}
+     */
+    static makeFieldsList(fields) {
+
+        /*
+         There was two arrays:
+
+         at componentDidMount:
+         ...fields.notEditableFields.map( (f) => { return {id: f.id, label: `${f.name} -- ${f.sourceName}`, type: f.valueType === 'float' ? 'double' : f.valueType} } ),
+         ...fields.sourceFieldsList.filter((f) => (f.sourceName !== 'sample')).map( (f) => { return {id: f.id, label: `${f.name} -- source`, type: f.valueType === 'float' ? 'double' : f.valueType }} )
+
+         at componentWillUpdate:
+         ...fields.sampleFieldsList.map( (f) => { return {id: f.id, label: `${f.label} -- ${f.sourceName}`, type: f.valueType === 'float' ? 'double' : f.valueType} } ),
+         ...fields.sourceFieldsList.filter((f) => (f.sourceName !== 'sample')).map( (f) => { return {id: f.id, label: `${f.label} -- source`, type: f.valueType === 'float' ? 'double' : f.valueType }} )
+
+         1st part is from 'notEditableFields' at 'componentDidMount' vs 'sampleFieldsList' at 'componentWillUpdate'
+         and all '.name' at componentDidMount vs '.label' at 'componentWillUpdate'
+
+         There must be not editable fields to prevent select gender for the person
+         */
+
+        return [
+            ...fields.notEditableFields.map( (f) => this.getFieldValue(f) ),
+            ...fields.sourceFieldsList.filter( (f) => (f.sourceName !== 'sample') ).map( (f) => this.getFieldValue(f, 'source') )
+        ];
+    }
 }
