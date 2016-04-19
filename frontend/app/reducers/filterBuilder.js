db958416d62b66dc99581bfa456e96635f53e388
dmitry <dmitry.zyryanov@execution.su>
2016-04-19 13:15:14 +0600

    filterBuilder refactor: functions extracted from 'switch'

--- frontend/app/reducers/filterBuilder.js
+++ frontend/app/reducers/filterBuilder.js
@@ -2,6 +2,76 @@ import * as ActionTypes from '../actions/filterBuilder'
 import {filterUtils, genomicsParsedRulesValidate, opsUtils} from '../utils/filterUtils';
 import FieldUtils from "../utils/fieldUtils";
 
+
+/**
+ * @param {boolean} isNew
+ * @param {{rules: {$and: ({id, label, type}|Object)[]=, $or: ({id, label, type}|Object)[]= }}} filterToEdit
+ * @param {{id: string, label: string, type: string}[]} fields
+ * @returns {{filter: {rules: {$and: ({id, label, type}|Object)[]=, $or: ({id, label, type}|Object)[]= }}, isNew: boolean, parsedFilter: {condition: string, rules: {condition: *=, field: string=, operator: string=, value: *=}[]}, fieldDefaultId: string}}
+ */
+function makeFilterEditing(isNew, filterToEdit, fields) {
+    const fieldDefaultId = FieldUtils.getDefaultId(fields);
+    const parsedRawRules = filterUtils.getRulesFromGenomics(filterToEdit.rules);
+    const validateRulesResult = genomicsParsedRulesValidate.validateGemonicsParsedRules(fields, parsedRawRules);
+    // Report validation results if any
+    if (validateRulesResult.report.length) {
+        console.error('Filter rules are invalid:');
+        console.error(JSON.stringify(parsedRawRules, null, 4));
+        console.error('Filter validation report:');
+        console.error(JSON.stringify(validateRulesResult.report, null, 4));
+    }
+    const parsedFilter = validateRulesResult.validRules ||
+        {condition : 'AND', rules: [{field: fieldDefaultId, operator: 'is_null'}]};
+    return {
+        filter: filterToEdit,
+        isNew,
+        parsedFilter,
+        fieldDefaultId
+    };
+}
+
+function makeFilterChanged(parsedFilter, fieldDefaultId, index, change) {
+    const modification = filterUtils.genomicsParsedRulesModification;
+    const changeFunctions = {
+        onSwitch(isAnd) {
+            return modification.switchCondition(parsedFilter, index, isAnd)
+        },
+        onEdit(itemTyped) {
+            const ruleIndex = itemTyped.ruleIndex;
+            const item = itemTyped.item;
+            const fieldJSType = itemTyped.fieldJSType;
+            const itemOpType = item.operator;
+            const itemOp = filterUtils.getOperatorByType(itemOpType);
+            const opWant = opsUtils.getOperatorWantedParams(itemOp);
+            const value = item.value;
+            const castedValue = opWant.noParams ?
+                null :
+                opWant.single ?
+                    (typeof value === 'object' && value && value.length) ? genomicsParsedRulesValidate.jsTypeCastValue(value.join(), fieldJSType) : genomicsParsedRulesValidate.jsTypeCastValue(value, fieldJSType) :
+                    genomicsParsedRulesValidate.jsTypeCastArray(value, fieldJSType, opWant.arraySize || 0);
+            return modification.setRule(parsedFilter, index, ruleIndex, {field: item.field, operator: item.operator, value: castedValue});
+        },
+        onDelete(itemIndex) {
+            return modification.removeRuleOrGroup(parsedFilter, index, itemIndex);
+        },
+        onAdd(isGroup) {
+            return modification.appendDefault(parsedFilter, index, isGroup, fieldDefaultId)
+        }
+    };
+    var changeName;
+    for (changeName in change) {
+        if (!change.hasOwnProperty(changeName)) {
+            continue;
+        }
+        const changeFunction = changeFunctions[changeName];
+        if (changeFunction) {
+            return changeFunction(change[changeName]);
+        }
+    }
+    return null;
+}
+
+
 export default function filterBuilder(state = {
     isReceivedFilters: false,
     selectedFilter: null,
@@ -16,7 +86,7 @@ export default function filterBuilder(state = {
 }, action) {
 
     var selectedFilter;
-    var filterToEdit;
+    var newParsedRules;
 
     switch (action.type) {
         case ActionTypes.FBUILDER_SELECT_FILTER:
@@ -27,73 +97,20 @@ export default function filterBuilder(state = {
             });
 
         case ActionTypes.FBUILDER_TOGGLE_NEW_EDIT:
-            filterToEdit = action.makeNew ?
-                Object.assign({}, state.selectedFilter, {
-                    type: 'user',
-                    name: `Copy of ${state.selectedFilter.name}`
-                }) :
-                state.selectedFilter;
-            const fields = FieldUtils.makeFieldsList(action.fields);
-            const fieldDefaultId = FieldUtils.getDefaultId(fields);
-            const parsedRawRules = filterUtils.getRulesFromGenomics(filterToEdit.rules);
-            const validateRulesResult = genomicsParsedRulesValidate.validateGemonicsParsedRules(fields, parsedRawRules);
-            // Report validation results if any
-            if (validateRulesResult.report.length) {
-                console.error('Filter rules are invalid:');
-                console.error(JSON.stringify(parsedRawRules, null, 4));
-                console.error('Filter validation report:');
-                console.error(JSON.stringify(validateRulesResult.report, null, 4));
-            }
-            const parsedRules = validateRulesResult.validRules ||
-                {condition : 'AND', rules: [{field: fieldDefaultId, operator: 'is_null'}]};
             return Object.assign({}, state, {
-                editingFilter: {
-                    filter: filterToEdit,
-                    isNew: action.makeNew,
-                    parsedFilter: parsedRules,
-                    fieldDefaultId: fieldDefaultId
-                }
+                editingFilter: makeFilterEditing(
+                    action.makeNew,
+                    action.makeNew ?
+                        Object.assign({}, state.selectedFilter, {
+                            type: 'user',
+                            name: `Copy of ${state.selectedFilter.name}`
+                        }) :
+                        state.selectedFilter,
+                    FieldUtils.makeFieldsList(action.fields))
             });
         
         case ActionTypes.FBUILDER_CHANGE_FILTER:
-            console.log(action);
-            const doFs = {
-                onSwitch(isAnd) {
-                    return filterUtils.genomicsParsedRulesModification.switchCondition(state.editingFilter.parsedFilter, action.index, isAnd)
-                },
-                onEdit(itemTyped) {
-                    const ruleIndex = itemTyped.ruleIndex;
-                    const item = itemTyped.item;
-                    const fieldJSType = itemTyped.fieldJSType;
-                    const itemOpType = item.operator;
-                    const itemOp = filterUtils.getOperatorByType(itemOpType);
-                    const opWant = opsUtils.getOperatorWantedParams(itemOp);
-                    const value = item.value;
-                    const castedValue = opWant.noParams ?
-                        null :
-                        opWant.single ?
-                            (typeof value === 'object' && value && value.length) ? genomicsParsedRulesValidate.jsTypeCastValue(value.join(), fieldJSType) : genomicsParsedRulesValidate.jsTypeCastValue(value, fieldJSType) :
-                            genomicsParsedRulesValidate.jsTypeCastArray(value, fieldJSType, opWant.arraySize || 0);
-                    return filterUtils.genomicsParsedRulesModification.setRule(state.editingFilter.parsedFilter, action.index, ruleIndex, {field: item.field, operator: item.operator, value: castedValue});
-                },
-                onDelete(itemIndex) {
-                    return filterUtils.genomicsParsedRulesModification.removeRuleOrGroup(state.editingFilter.parsedFilter, action.index, itemIndex);
-                },
-                onAdd(isGroup) {
-                    return filterUtils.genomicsParsedRulesModification.appendDefault(state.editingFilter.parsedFilter, action.index, isGroup, state.editingFilter.fieldDefaultId)
-                }
-            };
-            var newParsedRules;
-            for (var fname in action.change) {
-                if (!action.change.hasOwnProperty(fname)) {
-                    continue;
-                }
-                var doF = doFs[fname];
-                if (doF) {
-                    newParsedRules = doF(action.change[fname]);
-                    break;
-                }
-            }
+            newParsedRules = makeFilterChanged(state.editingFilter.parsedFilter, state.editingFilter.fieldDefaultId, action.index, action.change);
             if (!newParsedRules) {
                 return Object.assign({}, state, {});
             } else {
