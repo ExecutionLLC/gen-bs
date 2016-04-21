331f4bfae19f590268adcba0c53546e9ba3cef0f
Vasily Loginov <loginov.vasily@gmail.com>
2016-04-21 19:14:01 +0600

    Reformatted code.

--- frontend/app/components/Modals/FileUpload/FileUpload.js
+++ frontend/app/components/Modals/FileUpload/FileUpload.js
@@ -1,73 +1,75 @@
-import React, { Component } from 'react';
-import { Modal } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Modal} from 'react-bootstrap';
 import classNames from 'classnames';
 
 import FileUploadProgressBar from './FileUploadProgressBar';
 
-import { clearUploadState, changeFileForUpload } from '../../../actions/fileUpload';
+import {clearUploadState, changeFileForUpload} from '../../../actions/fileUpload';
 
 
 export default class FileUpload extends Component {
 
-  componentWillMount() {
-    this.props.dispatch(clearUploadState())
-  }
-
-  uploadClickHandler(event) {
-    this.refs.fileInput.click();
-  }
-
-  render() {
-    const { dispatch, auth } = this.props;
-    const { files, error, isArchiving } = this.props.fileUpload;
-    if (auth.isDemo){
-        return (
-            <div className="panel panel-primary">
-                <div className="panel-heading">
-                   Please login or register to upload new samples
-                </div>
-            </div>
-        );
-    } else {
-        return (
-            <div className="panel file-upload-panel panel-default">
-                <div className="panel-body">
-
-              { error &&
-                <div className="alert alert-danger">
-                  <p>{error}</p>
-                </div>
-              }
-
-                <button onClick={this.uploadClickHandler.bind(this)} data-target="#fileOpen" data-toggle="modal" className="btn-link-light-default btn-select-file" >
-                  <input
-                    onChange={ (e) => dispatch(changeFileForUpload(e.target.files))}
-                    style={{display: 'none'}}
-                    ref="fileInput"
-                    id="file-select"
-                    type="file"
-                    accept=".vcf,.gz"
-                    name="files[]"
-                  />
-                     <i className="md-i">cloud_upload</i>
-                     <span>Drop some files</span>
-                     <small>Or click here</small>
-                     <span>.vcf, .vcf.gz</span>
-                </button>
-                { files[0] &&
-                  <div className="text-center"><strong style={{color: '#2363a1'}}>{files[0].name}</strong></div>
-                }
-            
-
-            { isArchiving &&
-              <div className="text-center"><strong style={{color: '#2363a1'}}>Archiving...</strong><i className="fa fa-spinner fa-spin"></i></div>
-            }
-            { !error &&
-              <FileUploadProgressBar {...this.props} />
-            }
-            </div>
-          </div>
-        );
+    componentWillMount() {
+        this.props.dispatch(clearUploadState())
     }
-  }
+
+    uploadClickHandler(event) {
+        this.refs.fileInput.click();
+    }
+
+    render() {
+        const {dispatch, auth} = this.props;
+        const {files, error, isArchiving} = this.props.fileUpload;
+        if (auth.isDemo) {
+            return (
+                <div className="panel panel-primary">
+                    <div className="panel-heading">
+                        Please login or register to upload new samples
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="panel file-upload-panel panel-default">
+                    <div className="panel-body">
+
+                        { error &&
+                        <div className="alert alert-danger">
+                            <p>{error}</p>
+                        </div>
+                        }
+
+                        <button onClick={this.uploadClickHandler.bind(this)} data-target="#fileOpen" data-toggle="modal"
+                                className="btn-link-light-default btn-select-file">
+                            <input
+                                onChange={ (e) => dispatch(changeFileForUpload(e.target.files))}
+                                style={{display: 'none'}}
+                                ref="fileInput"
+                                id="file-select"
+                                type="file"
+                                accept=".vcf,.gz"
+                                name="files[]"
+                            />
+                            <i className="md-i">cloud_upload</i>
+                            <span>Drop some files</span>
+                            <small>Or click here</small>
+                            <span>.vcf, .vcf.gz</span>
+                        </button>
+                        { files[0] &&
+                        <div className="text-center"><strong style={{color: '#2363a1'}}>{files[0].name}</strong></div>
+                        }
+
+
+                        { isArchiving &&
+                        <div className="text-center"><strong style={{color: '#2363a1'}}>Archiving...</strong><i
+                            className="fa fa-spinner fa-spin"></i></div>
+                        }
+                        { !error &&
+                        <FileUploadProgressBar {...this.props} />
+                        }
+                    </div>
+                </div>
+            );
+        }
+    }
 }
