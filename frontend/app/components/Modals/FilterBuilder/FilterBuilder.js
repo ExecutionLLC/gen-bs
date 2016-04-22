6680e3a051901d9532c09e9d204b578894568e01
dmitry <dmitry.zyryanov@execution.su>
2016-04-22 19:22:40 +0600

    merge fix

--- frontend/app/components/Modals/FilterBuilder/FilterBuilder.js
+++ frontend/app/components/Modals/FilterBuilder/FilterBuilder.js
@@ -431,61 +431,6 @@ export default class FilterBuilder extends React.Component {
             || this.props.filterBuilder !== nextProps.filterBuilder;
     }
 
-    disableFilter(filter, el) {
-        window.$(el).queryBuilder('setRulesFromGenomics', filter.rules);
-        if (filter.type === 'standard' || filter.type == 'advanced') {
-            //inputs and selects
-            window.$('input[name*="builder-basic_rule"],select[name*="builder-basic_rule"]')
-                .prop('disabled', true);
-            //and, or operators
-            window.$('div[class*="group-conditions"]')
-                .children()
-                .children()
-                .prop('disabled', true);
-            //add rule ,add group
-            window.$('div[class*="group-actions"]')
-                .children()
-                .prop('disabled', true);
-            window.$('div[class*="rule-actions"]')
-                .children()
-                .prop('disabled', true);
-        }
-    }
-
-    componentWillUpdate(nextProps) {
-        const {dispatch, fields} = nextProps;
-        const {editOrNew, rulesRequested, editedFilter, newFilter} = nextProps.filterBuilder;
-        const filter = editOrNew ? (editedFilter) : (newFilter);
-        var el = this.refs.builder;
-        var rules = [];
-        const builderFilters = [
-
-            ...fields.sampleFieldsList.map((f) => {
-                return {
-                    id: f.id,
-                    label: `${f.label} -- ${f.sourceName}`,
-                    type: f.valueType === 'float' ? 'double' : f.valueType
-                }
-            }),
-            ...fields.sourceFieldsList.filter((f) => (f.sourceName !== 'sample')).map((f) => {
-                return {id: f.id, label: `${f.label} -- source`, type: f.valueType === 'float' ? 'double' : f.valueType}
-            })
-
-        ];
-
-        if (rulesRequested) {
-            rules = window.$(el).queryBuilder('getGenomics');
-            console.log('rules', JSON.stringify(rules));
-            dispatch(filterBuilderReceiveRules(rules))
-        } else {
-            window.$(el).queryBuilder({
-                filters: builderFilters,
-                operators: filterOperators
-            });
-            this.disableFilter(filter, el);
-        }
-    }
-
     render() {
         const {
             filterBuilder,
