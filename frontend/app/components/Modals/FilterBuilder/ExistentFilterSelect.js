0a09339a068c49a486149637d352a8001165587a
elenabaranova <e.elfairy@gmail.com>
2016-04-23 17:32:19 +0300

    responsive class for delete button

--- frontend/app/components/Modals/FilterBuilder/ExistentFilterSelect.js
+++ frontend/app/components/Modals/FilterBuilder/ExistentFilterSelect.js
@@ -86,7 +86,7 @@ export default class ExistentFilterSelect extends Component {
                             <button type="button"
                                     className="btn btn-default"
                                     onClick={ () => dispatch(filterBuilderDeleteFilter(selectedFilter.id))}>
-                                <span data-localize="views.setup.delete.title">Delete Filter</span>
+                                <span data-localize="views.setup.delete.title" className="hidden-xs">Delete Filter</span>
                                 <span className="visible-xs"><i className="md-i">close</i></span>
                             </button>
                         </div>
