a7f8720c67e76ef63419b7e47b7157ec0dfaf772
Vasily Loginov <loginov.vasily@gmail.com>
2016-04-26 19:18:55 +0600

    Moved fields' reducers into separate functions.

--- frontend/app/reducers/fields.js
+++ frontend/app/reducers/fields.js
@@ -7,6 +7,60 @@ function updateFieldLabelIfNeeded(field) {
     });
 }
 
+function reduceRequestFields(action, state) {
+    return Object.assign({}, state, {
+        isFetching: Object.assign({}, state.isFetching, {
+            samples: true
+        })
+    });
+}
+
+function reduceReceiveFields(action, state) {
+    const fields = action.fields.map(updateFieldLabelIfNeeded);
+    const editableFields = _.filter(fields, 'isEditable', true);
+    const notEditableSampleFields = _.filter(fields, 'isEditable', false);
+    const idToFieldHash = _.reduce(fields, (result, field) => {
+        result[field.id] = field;
+        return result;
+    }, {});
+
+    return Object.assign({}, state, {
+        isFetching: Object.assign({}, state.isFetching, {
+            samples: false
+        }),
+        sampleFieldsList: fields,
+        editableFields,
+        notEditableFields: notEditableSampleFields,
+        idToFieldHash,
+        lastUpdated: action.receivedAt
+    });
+}
+
+function reduceReceiveTotalFields(action, state) {
+    let totalFields = action.fields.map(updateFieldLabelIfNeeded);
+    let sourceFields = _.filter(totalFields, (field) => field.sourceName !== 'sample');
+    return Object.assign({}, state, {
+        isFetching: Object.assign({}, state.isFetching, {
+            sources: false
+        }),
+        totalFieldsList: totalFields,
+        totalFieldsHash: _.reduce(totalFields, (result, field) => {
+            result[field.id] = field;
+            return result;
+        }, {}),
+        sourceFieldsList: sourceFields,
+        lastUpdated: action.receivedAt
+    });
+}
+
+function reduceRequestTotalFields(action, state) {
+    return Object.assign({}, state, {
+        isFetching: Object.assign({}, state.isFetching, {
+            sources: true
+        })
+    });
+}
+
 export default function fields(state = {
     isFetching: {samples: false, sources: false},
     sampleFieldsList: [],
@@ -18,53 +72,16 @@ export default function fields(state = {
     switch (action.type) {
 
         case ActionTypes.REQUEST_FIELDS:
-            return Object.assign({}, state, {
-                isFetching: Object.assign({}, state.isFetching, {
-                    samples: true
-                })
-            });
+            return reduceRequestFields(action, state);
 
         case ActionTypes.RECEIVE_FIELDS:
-            const fields = action.fields.map(updateFieldLabelIfNeeded);
-            const editableFields = _.filter(fields, 'isEditable', true);
-            const notEditableSampleFields = _.filter(fields, 'isEditable', false);
-            const idToFieldHash = _.reduce(fields, (result, field) => {
-                result[field.id] = field;
-                return result;
-            }, {});
-            return Object.assign({}, state, {
-                isFetching: Object.assign({}, state.isFetching, {
-                    samples: false
-                }),
-                sampleFieldsList: fields,
-                editableFields,
-                notEditableFields:notEditableSampleFields,
-                idToFieldHash,
-                lastUpdated: action.receivedAt
-            });
+            return reduceReceiveFields(action, state);
 
         case ActionTypes.REQUEST_TOTAL_FIELDS:
-            return Object.assign({}, state, {
-                isFetching: Object.assign({}, state.isFetching, {
-                    sources: true
-                })
-            });
+            return reduceRequestTotalFields(action, state);
 
         case ActionTypes.RECEIVE_TOTAL_FIELDS:
-            let totalFields = action.fields.map(updateFieldLabelIfNeeded);
-            let sourceFields = _.filter(totalFields, (field) => field.sourceName !== 'sample');
-            return Object.assign({}, state, {
-                isFetching: Object.assign({}, state.isFetching, {
-                    sources: false
-                }),
-                totalFieldsList: totalFields,
-                totalFieldsHash: _.reduce(totalFields, (result, field) => {
-                    result[field.id] = field;
-                    return result;
-                }, {}),
-                sourceFieldsList: sourceFields,
-                lastUpdated: action.receivedAt
-            });
+            return reduceReceiveTotalFields(action, state);
 
         default:
             return state;
