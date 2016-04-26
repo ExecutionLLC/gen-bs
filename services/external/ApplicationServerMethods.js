5fddcb7a3e6e4bd77602d76b78ba3d6acee72859
Vasily Loginov <loginov.vasily@gmail.com>
2016-04-26 15:15:10 +0600

    Extracted AS methods into a separate file.

---
+++ services/external/ApplicationServerMethods.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    getSourcesList: 'v1.get_sources',
+    getSourceMetadata: 'v1.get_source_metadata',
+    checkSession: 'v1.get_session_state',
+    keepAlive: 'v1.keep_alive',
+    openSearchSession: 'v1.open_session',
+    closeSession: 'v1.close_session',
+    searchInResults: 'v1.search_in_results',
+    uploadSample: 'v1.upload_file',
+    processSample: 'v1.convert_file'
+};
--- services/external/ApplicationServerService.js
+++ services/external/ApplicationServerService.js
@@ -10,17 +10,7 @@ const AppServerViewUtils = require('../../utils/AppServerViewUtils');
 const AppServerFilterUtils = require('../../utils/AppServerFilterUtils');
 const AppServerUploadUtils = require('../../utils/AppServerUploadUtils');
 
-const METHODS = {
-    getSourcesList: 'v1.get_sources',
-    getSourceMetadata: 'v1.get_source_metadata',
-    checkSession: 'v1.get_session_state',
-    keepAlive: 'v1.keep_alive',
-    openSearchSession: 'v1.open_session',
-    closeSession: 'v1.close_session',
-    searchInResults: 'v1.search_in_results',
-    uploadSample: 'v1.upload_file',
-    processSample: 'v1.convert_file'
-};
+const METHODS = require('./ApplicationServerMethods');
 
 /**
  * This service sends requests to AS.
