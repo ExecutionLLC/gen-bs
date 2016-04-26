ea7c911a33bc8b2890008c99c3c7cbdb5b9c5fd5
Vasily Loginov <loginov.vasily@gmail.com>
2016-04-26 18:51:26 +0600

    Extracted event handlers to separate functions inside the input control. (#265)

--- frontend/app/components/shared/Input.js
+++ frontend/app/components/shared/Input.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 
 /**
  * Input field component with on blur and on enter firing onChange
@@ -19,18 +19,53 @@ export default class Input extends Component {
 
     render() {
         const value = this.state.value;
-        const {onChange, onChanging} = this.props;
-
         return (
             <input
                 {...this.props}
                 value={value}
-                onChange={ (evt) => { const value = evt.target.value; this.setState({value: value}); if (onChanging) onChanging(value); } }
-                onBlur={ (evt) => onChange(evt.target.value) }
-                onKeyDown={ (evt) => { if (evt.keyCode == 13) onChange(value) } }
+                onChange={ (evt) => this.onInputChanged(evt) }
+                onBlur={ (evt) => this.onInputBlur(evt) }
+                onKeyDown={ (evt) => this.onInputKeyDown(evt)  }
             />
         );
     }
+
+    onInputBlur(evt) {
+        const {onChange} = this.props;
+        onChange(evt.target.value);
+    }
+
+    onInputKeyDown(evt) {
+        if (evt.keyCode !== 13) {
+            return;
+        }
+
+        const {value} = this.state;
+        const {onChange} = this.props;
+        onChange(value);
+    }
+
+    onInputChanged(evt) {
+        const {onChanging} = this.props;
+        const value = evt.target.value;
+
+        this.setState({value});
+
+        if (onChanging) {
+            onChanging(value);
+        }
+    }
+
+    static propTypes = {
+        /**
+         * @type function(string)
+         */
+        onChange: PropTypes.func.isRequired,
+        /**
+         * @type function(string)
+         */
+        onChanging: PropTypes.func
+    };
 }
 
 /**
