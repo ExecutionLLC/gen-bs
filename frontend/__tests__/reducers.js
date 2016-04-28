83881f14d743deed735240575616b71b1b03d4dd
dmitry <dmitry.zyryanov@execution.su>
2016-04-28 13:29:29 +0600

    jest: clearer way, named tests

--- frontend/__tests__/reducers.js
+++ frontend/__tests__/reducers.js
@@ -4,170 +4,163 @@ import {toggleQueryNavbar, changeView, changeHeaderFilter, changeFilter} from '.
 import {receiveViews} from '../app/actions/userData';
 import configureStore from '../app/store/configureStore';
 
-/**
- * @template {T}
- * @param {{exec: function(), state: T}[]} tests
- * @param {function():T} getState
- * @param {function()} done
- * @returns {function()}
- */
-function makeStateTests(tests, getState, done) {
+function makeStateTests(tests, store, getState) {
 
-    var currentTest = null;
-
-    function doTest() {
-        currentTest = tests.shift();
-        if (!currentTest) {
-            done();
-            return;
+    var stateChangeF;
+    const unsubscribe = store.subscribe(() => {
+        if (stateChangeF) {
+            var f = stateChangeF;
+            stateChangeF = null;
+            f();
         }
-        currentTest.exec();
+    });
+
+    function doTest(i) {
+        const test = tests[i];
+        it(test.name, (done) => {
+            if (test.exec) {
+                stateChangeF = () => {
+                    expect(getState()).toEqual(test.state);
+                    done();
+                };
+                test.exec();
+            } else {
+                expect(getState()).toEqual(test.state);
+                done();
+            }
+        });
     }
 
-    function testCheck() {
-        expect(getState()).toEqual(currentTest.state);
+    var i;
+    for (i = 0; i < tests.length; i++) {
+        doTest(i);
     }
-
-    doTest.check = testCheck;
-    return doTest;
+    it('no-test-state-listener-unsubscribe', () => unsubscribe() );
 }
 
-
 describe('dispatching errorHandler actions', () => {
-    it('succeed when', (done) => {
 
-        const tests = [
-            {
-                name: 'init',
-                exec: () => {},
-                state: {showErrorWindow: false, lastError: null}
+    const tests = [
+        {
+            name: 'init',
+            exec: null,
+            state: {showErrorWindow: false, lastError: null}
+        },
+        {
+            name: 'error set 1',
+            exec: () => {
+                store.dispatch(handleError(1, 'q'));
             },
-            {
-                name: 'error set',
-                exec: () => { store.dispatch(handleError(1, 'q')); },
-                state: {showErrorWindow: true, lastError: {errorCode: 1, errorMessage: 'q'}}
+            state: {showErrorWindow: true, lastError: {errorCode: 1, errorMessage: 'q'}}
+        },
+        {
+            name: 'error set 2',
+            exec: () => {
+                store.dispatch(handleError(2, 'w'));
             },
-            {
-                name: 'error set',
-                exec: () => { store.dispatch(handleError(2, 'w')); },
-                state: {showErrorWindow: true, lastError: {errorCode: 2, errorMessage: 'w'}}
+            state: {showErrorWindow: true, lastError: {errorCode: 2, errorMessage: 'w'}}
+        },
+        {
+            name: 'error reset',
+            exec: () => {
+                store.dispatch(lastErrorResolved());
             },
-            {
-                name: 'error reset',
-                exec: () => { store.dispatch(lastErrorResolved()); },
-                state: {showErrorWindow: false, lastError: null }
-            }
-        ];
+            state: {showErrorWindow: false, lastError: null}
+        }
+    ];
 
-        const store = configureStore();
-        var unsubscribe;
-        var t = makeStateTests(tests, () => store.getState().errorHandler, () => { unsubscribe(); done(); } );
-        unsubscribe = store.subscribe( () => {
-            t.check();
-            t();
-        });
-        t();
-        t.check();
-        t();
-    });
+    const store = configureStore();
+    makeStateTests(tests, store, () => store.getState().errorHandler);
+    
 });
 
 describe('dispatching modalWindows actions', () => {
-    it('succeed when', (done) => {
+    const tests = [
+        {
+            name: 'init',
+            state: {views: {showModal: false}, filters: {showModal: false}, upload: {showModal: false}}
+        },
+        {
+            name: 'close already closed filters',
+            exec: () => { store.dispatch(closeModal('filters')); },
+            state: {views: {showModal: false}, filters: {showModal: false}, upload: {showModal: false}}
+        },
+        {
+            name: 'open upload',
+            exec: () => { store.dispatch(openModal('upload')); },
+            state: {views: {showModal: false}, filters: {showModal: false}, upload: {showModal: true}}
+        },
+        {
+            name: 'open views',
+            exec: () => { store.dispatch(openModal('views')); },
+            state: {views: {showModal: true}, filters: {showModal: false}, upload: {showModal: true}}
+        },
+        {
+            name: 'open filters',
+            exec: () => { store.dispatch(openModal('filters')); },
+            state: {views: {showModal: true}, filters: {showModal: true}, upload: {showModal: true}}
+        },
+        {
+            name: 'close views',
+            exec: () => { store.dispatch(closeModal('views')); },
+            state: {views: {showModal: false}, filters: {showModal: true}, upload: {showModal: true}}
+        },
+        {
+            name: 'close upload',
+            exec: () => { store.dispatch(closeModal('upload')); },
+            state: {views: {showModal: false}, filters: {showModal: true}, upload: {showModal: false}}
+        },
+        {
+            name: 'open already opened filters',
+            exec: () => { store.dispatch(openModal('filters')); },
+            state: {views: {showModal: false}, filters: {showModal: true}, upload: {showModal: false}}
+        }
+    ];
 
-        const tests = [
-            {
-                exec: () => {},
-                state: {views: {showModal: false}, filters: {showModal: false}, upload: {showModal: false}}
-            },
-            {
-                exec: () => { store.dispatch(closeModal('filters')); },
-                state: {views: {showModal: false}, filters: {showModal: false}, upload: {showModal: false}}
-            },
-            {
-                exec: () => { store.dispatch(openModal('upload')); },
-                state: {views: {showModal: false}, filters: {showModal: false}, upload: {showModal: true}}
-            },
-            {
-                exec: () => { store.dispatch(openModal('views')); },
-                state: {views: {showModal: true}, filters: {showModal: false}, upload: {showModal: true}}
-            },
-            {
-                exec: () => { store.dispatch(openModal('filters')); },
-                state: {views: {showModal: true}, filters: {showModal: true}, upload: {showModal: true}}
-            },
-            {
-                exec: () => { store.dispatch(closeModal('views')); },
-                state: {views: {showModal: false}, filters: {showModal: true}, upload: {showModal: true}}
-            },
-            {
-                exec: () => { store.dispatch(closeModal('upload')); },
-                state: {views: {showModal: false}, filters: {showModal: true}, upload: {showModal: false}}
-            },
-            {
-                exec: () => { store.dispatch(openModal('filters')); },
-                state: {views: {showModal: false}, filters: {showModal: true}, upload: {showModal: false}}
-            }
-        ];
-
-        const store = configureStore();
-        var unsubscribe;
-        const t = makeStateTests(tests, () => store.getState().modalWindows, () => { unsubscribe(); done() } );
-        unsubscribe = store.subscribe( () => {
-            t.check();
-            t();
-        });
-        t();
-        t.check();
-        t();
-    });
+    const store = configureStore();
+    makeStateTests(tests, store, () => store.getState().modalWindows);
 });
 
 describe('dispatching ui actions', () => {
-    it('succeed when', (done) => {
 
-        const INIT_STATE = {queryNavbarClosed: true, selectedView: null, selectedFilter: null, currentLimit: 100, currentOffset: 0, isAnalyzeTooltipVisible: false, language: 'en'};
+    const INIT_STATE = {queryNavbarClosed: true, selectedView: null, selectedFilter: null, currentLimit: 100, currentOffset: 0, isAnalyzeTooltipVisible: false, language: 'en'};
 
-        function chstate(ch) {
-            return Object.assign({}, INIT_STATE, ch);
+    function chstate(ch) {
+        return Object.assign({}, INIT_STATE, ch);
+    }
+
+    const tests = [
+        {
+            name: 'init',
+            state: INIT_STATE
+        },
+        {
+            name: 'toggle query navbar',
+            exec: () => store.dispatch(toggleQueryNavbar()),
+            state: chstate({queryNavbarClosed: ''+false})
+        },
+        {
+            name: 'toggle query navbar again',
+            exec: () => store.dispatch(toggleQueryNavbar()),
+            state: chstate()
+        },
+        {
+            name: 'set views',
+            exec: () => store.dispatch(receiveViews([{id:1, q:2}, {id:2, w:3}])),
+            state: chstate()
+        },
+        {
+            name: 'change absent view',
+            exec: () => store.dispatch(changeView(4)),
+            state: chstate({selectedView: void 0})
+        },
+        {
+            name: 'change existent view',
+            exec: () => store.dispatch(changeView(2)),
+            state: chstate({selectedView: {id:2, w:3}})
         }
+    ];
 
-        const tests = [
-            {
-                exec: () => {},
-                state: INIT_STATE
-            },
-            {
-                exec: () => store.dispatch(toggleQueryNavbar()),
-                state: chstate({queryNavbarClosed: false})
-            },
-            {
-                exec: () => store.dispatch(toggleQueryNavbar()),
-                state: chstate()
-            },
-            {
-                exec: () => store.dispatch(receiveViews([{id:1, q:2}, {id:2, w:3}])),
-                state: chstate()
-            },
-            {
-                exec: () => store.dispatch(changeView(4)),
-                state: chstate({selectedView: void 0})
-            },
-            {
-                exec: () => store.dispatch(changeView(2)),
-                state: chstate({selectedView: {id:2, w:3}})
-            }
-        ];
-
-        const store = configureStore();
-        var unsubscribe;
-        const t = makeStateTests(tests, () => store.getState().ui, () => { unsubscribe(); done() } );
-        unsubscribe = store.subscribe( () => {
-            t.check();
-            t();
-        });
-        t();
-        t.check();
-        t();
-    });
+    const store = configureStore();
+    makeStateTests(tests, store, () => store.getState().ui);
 });
