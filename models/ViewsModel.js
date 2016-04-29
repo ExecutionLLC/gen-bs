c2247698bdd84f04bd200d578b7a89b6a2af5c48
dmitry <dmitry.zyryanov@execution.su>
2016-04-29 15:29:58 +0600

    Revert "issue 293: backend check if view fields are not empty"
    This reverts commit 58cf042b57848c773f686b9ee0e02707100a5a42.
    
    There's a better check in single place

--- models/ViewsModel.js
+++ models/ViewsModel.js
@@ -81,10 +81,6 @@ class ViewsModel extends SecureModelBase {
                     });
                 },
                 (viewId, callback) => {
-                    if (!view.viewListItems || !view.viewListItems.length) {
-                        callback(new Error("Can't add view without fields"));
-                        return;
-                    }
                     this._addViewItems(viewId, view.viewListItems, trx, (error) => {
                         callback(error, viewId);
                     });
@@ -164,10 +160,6 @@ class ViewsModel extends SecureModelBase {
                     });
                 },
                 (viewId, callback) => {
-                    if (!viewToUpdate.viewListItems || !viewToUpdate.viewListItems.length) {
-                        callback(new Error("Can't update view without fields"));
-                        return;
-                    }
                     this._addViewItems(viewId, viewToUpdate.viewListItems, trx, (error) => {
                         callback(error, viewId);
                     });
