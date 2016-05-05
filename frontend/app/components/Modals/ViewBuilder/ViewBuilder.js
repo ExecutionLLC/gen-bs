50c6b76fcfab6992fef83ee2cddaa4c426945f60
Vasily Loginov <loginov.vasily@gmail.com>
2016-05-05 16:35:47 +0600

    Now checking if there is a field available. (#293)

--- frontend/app/components/Modals/ViewBuilder/ViewBuilder.js
+++ frontend/app/components/Modals/ViewBuilder/ViewBuilder.js
@@ -18,17 +18,6 @@ export default class ViewBuilder extends React.Component {
 
     render() {
         const {dispatch, fields, viewBuilder} = this.props;
-        const view = viewBuilder.editedView;
-        const viewItemsLength = view.viewListItems.length;
-        var disabledClass = classNames({
-            'disabled': (view.type !== 'user' || viewItemsLength >= 30) ? 'disabled' : ''
-        });
-        var disabledClassMinus = classNames({
-            'disabled': (view.type !== 'user' || viewItemsLength <= 1) ? 'disabled' : ''
-        });
-
-        const previouslySelectedFieldIds = view.viewListItems.map(viewItem => viewItem.fieldId);
-        const isDisableEditing = view.type !== 'user';
         const allAvailableFields = fields.allowedFieldsList;
         // Exclude fields that are already selected.
         const fieldsForSelection = _.filter(
@@ -37,6 +26,17 @@ export default class ViewBuilder extends React.Component {
         );
         // This field will be chosen when a new item is created.
         const nextDefaultField = _.first(fieldsForSelection);
+        const view = viewBuilder.editedView;
+        const viewItemsLength = view.viewListItems.length;
+        var plusDisabledClass = classNames({
+            'disabled': (view.type !== 'user' || viewItemsLength >= 30 || !nextDefaultField) ? 'disabled' : ''
+        });
+        var minusDisabledClass = classNames({
+            'disabled': (view.type !== 'user' || viewItemsLength <= 1) ? 'disabled' : ''
+        });
+
+        const previouslySelectedFieldIds = view.viewListItems.map(viewItem => viewItem.fieldId);
+        const isDisableEditing = view.type !== 'user';
         const selects = view.viewListItems.map(function (viewItem, index) {
 
             var currentValue =
@@ -98,11 +98,11 @@ export default class ViewBuilder extends React.Component {
                     </div>
 
                     <div className="col-xs-1">
-                        <button className="btn-link" disabled={disabledClassMinus}
+                        <button className="btn-link" disabled={minusDisabledClass}
                                 onClick={ () => dispatch(viewBuilderDeleteColumn(index)) }
                                 type="button">
                             <i className="fa fa-lg fa-minus-circle"/></button>
-                        <button className="btn-link" disabled={disabledClass}
+                        <button className="btn-link" disabled={plusDisabledClass}
                                 onClick={ () => dispatch(viewBuilderAddColumn(index+1, nextDefaultField.id)) }
                                 type="button">
                             <i className="fa fa-lg fa-plus-circle"/></button>
