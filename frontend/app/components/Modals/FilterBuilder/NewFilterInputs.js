2bbb96924eedca563c227b2440034e68d9d3fa0a
ponomarevvl90 <ponomarev.vl90@gmail.com>
2016-05-11 15:08:32 +0600

    add info label during creation

--- frontend/app/components/Modals/FilterBuilder/NewFilterInputs.js
+++ frontend/app/components/Modals/FilterBuilder/NewFilterInputs.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
-import { Modal } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Modal} from 'react-bootstrap';
 
-import { filterBuilderChangeAttr, filterBuilderToggleNewEdit } from '../../../actions/filterBuilder'
+import {filterBuilderChangeAttr, filterBuilderToggleNewEdit} from '../../../actions/filterBuilder'
 
 
 export default class NewFilterInputs extends Component {
 
     render() {
-        const { dispatch, showModal, closeModal, fields } = this.props
-        const { filters } = this.props.userData
+        const {dispatch, showModal, closeModal, fields} = this.props;
+        const {filters} = this.props.userData;
         const editingFilter = this.props.filterBuilder.editingFilter.filter;
+
+        const filterNameExists = _.some(filters, filter => filter.name == editingFilter.name);
+        const descriptionText = (filterNameExists) ? 'Filter with this name is already exists.' : '';
 
         return (
 
             <div className="collapse in copyview">
+                { descriptionText &&
+                <div className="alert alert-help">
+                        <span data-localize="views.setup.selector.description">
+                            {descriptionText}
+                        </span>
+                </div>
+                }
                 <div className="row grid-toolbar">
 
                     <div className="col-sm-6">
