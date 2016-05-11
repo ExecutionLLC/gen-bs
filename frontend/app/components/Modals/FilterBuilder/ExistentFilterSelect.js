154a8a759c21138818cbcb7c8b282719e4212c5e
dmitry <dmitry.zyryanov@execution.su>
2016-05-11 17:07:45 +0600

    filter builder edit refactor: extract UI handlers and other calculations from layout

--- frontend/app/components/Modals/FilterBuilder/ExistentFilterSelect.js
+++ frontend/app/components/Modals/FilterBuilder/ExistentFilterSelect.js
@@ -15,6 +15,22 @@ import {
 
 export default class ExistentFilterSelect extends Component {
 
+    onSelectChange(filters, filterId, fields) {
+        this.props.dispatch(filterBuilderStartEdit(false, filters.find((filter) => filter.id === filterId) || null, fields));
+    }
+
+    onDuplicateClick(filter, fields) {
+        this.props.dispatch(filterBuilderStartEdit(true, filter, fields));
+    }
+
+    onResetFilterClick(filter, fields) {
+        this.props.dispatch(filterBuilderStartEdit(false, filter, fields));
+    }
+
+    onDeleteFilterClick(filterId) {
+        this.props.dispatch(filterBuilderDeleteFilter(filterId));
+    }
+
     render() {
 
         const {dispatch, auth, fields} = this.props;
@@ -27,7 +43,14 @@ export default class ExistentFilterSelect extends Component {
         const isFilterEditable = (selectedFilter.type === 'user');
 
         const descriptionText = getReadonlyReasonForSessionAndType('filter', auth.isDemo, selectedFilter.type);
-        
+
+        const selectItems = filters.map( filter => {
+            return {
+                value: filter.id,
+                label: getItemLabelByNameAndType(filter.name, filter.type)
+            }
+        });
+
         return (
 
             <div className="in copyview">
@@ -46,11 +69,9 @@ export default class ExistentFilterSelect extends Component {
                 <div className="row grid-toolbar row-head-selector">
                     <div className="col-sm-6">
                         <Select
-                            options={filters.map( filter => { return {value: filter.id, label: getItemLabelByNameAndType(filter.name, filter.type)} } )}
+                            options={selectItems}
                             value={selectedFilter.id}
-                            onChange={ (val) => {
-                                dispatch(filterBuilderStartEdit(false, filters.find((filter) => filter.id === val.value) || null, fields));
-                            }}
+                            onChange={(val) => this.onSelectChange(filters, val.value, fields)}
                         />
                     </div>
                     <div className="col-sm-6">
@@ -59,7 +80,7 @@ export default class ExistentFilterSelect extends Component {
                             <button type="button"
                                     className="btn btn-default in copyview"
                                     id="dblBtn"
-                                    onClick={ () => dispatch(filterBuilderStartEdit(true, selectedFilter, fields)) }
+                                    onClick={() => this.onDuplicateClick(selectedFilter, fields)}
                                     disabled={disabledClass}
                                     title={title}
                             >
@@ -71,9 +92,7 @@ export default class ExistentFilterSelect extends Component {
                             }
                             { isFilterEditable &&
                                 <button type="button" className="btn btn-default"
-                                        onClick={() => {
-                                            dispatch(filterBuilderStartEdit(false, selectedFilter, fields));
-                                        }}
+                                        onClick={() => this.onResetFilterClick(selectedFilter, fields)}
                                 >
                                     <span data-localize="views.setup.reset.title" className="hidden-xs">Reset Filter</span>
                                     <span className="visible-xs"><i className="md-i">settings_backup_restore</i></span>
@@ -82,7 +101,7 @@ export default class ExistentFilterSelect extends Component {
                             { isFilterEditable &&
                                 <button type="button"
                                         className="btn btn-default"
-                                        onClick={ () => dispatch(filterBuilderDeleteFilter(selectedFilter.id))}>
+                                        onClick={() => this.onDeleteFilterClick(selectedFilter.id)}>
                                     <span data-localize="views.setup.delete.title" className="hidden-xs">Delete Filter</span>
                                     <span className="visible-xs"><i className="md-i">close</i></span>
                                 </button>
