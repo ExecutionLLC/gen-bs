246d1706c8ca486707d0ea79fab195a04de8a3f2
dmitry <dmitry.zyryanov@execution.su>
2016-05-17 15:28:41 +0600

    linter fixes: indents fixed

--- frontend/app/components/Header/NavbarMain/CreateQueryNavbarButton.js
+++ frontend/app/components/Header/NavbarMain/CreateQueryNavbarButton.js
@@ -23,21 +23,23 @@ export default class CreateQeuryNavbarButton extends Component {
 
     render() {
         return (
-      <div>
-         <div>
-           <a onClick={this.props.toggleQueryNavbar} type='button' href='#' className='btn navbar-btn' id='btnToggle'>
-              <span className='hidden-xxs' data-localize='query.help' data-toggle='tooltip' data-placement='right' title='Open navbar and create new analises query' data-container='body' data-trigger='hover'>Analyze</span>
-              <span className='visible-xxs'><i className='md-i'>settings</i></span>
-         </a>
-             {this.enablePerformanceControls &&
-                 <div>
-                     <button onClick={() => Perf.start()}>B</button>
-                     <button onClick={() => this.onStop()}>E</button>
-                 </div>
-             }
-         </div>
-      </div>
-
-    );
+            <div>
+                <div>
+                    <a onClick={this.props.toggleQueryNavbar} type='button' href='#' className='btn navbar-btn'
+                       id='btnToggle'>
+                        <span className='hidden-xxs' data-localize='query.help' data-toggle='tooltip'
+                              data-placement='right' title='Open navbar and create new analises query'
+                              data-container='body' data-trigger='hover'>Analyze</span>
+                        <span className='visible-xxs'><i className='md-i'>settings</i></span>
+                    </a>
+                    {this.enablePerformanceControls &&
+                    <div>
+                        <button onClick={() => Perf.start()}>B</button>
+                        <button onClick={() => this.onStop()}>E</button>
+                    </div>
+                    }
+                </div>
+            </div>
+        );
     }
 }
