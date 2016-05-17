a106415e0252dfc3b28d76e010751586ec60b1c7
dmitry <dmitry.zyryanov@execution.su>
2016-05-17 14:54:08 +0600

    linter fixes: indents and empty lines fixed

--- frontend/app/components/Header/NavbarCreateQuery/Analyze.js
+++ frontend/app/components/Header/NavbarCreateQuery/Analyze.js
@@ -18,23 +18,22 @@ export default class Analyze extends Component {
             target: () => ReactDOM.findDOMNode(this.refs.analyze_tooltip_target)
 
         };
+
         return (
-
-        <div className='table-cell table-cell-xs-4'>
-            <div className='btn-group'
-              data-localize='query.analyze.help'
-              ref='analyze_tooltip_target'
-              >  
-              <button className='btn btn-primary' type='button' disabled={isFetchingSamples} onClick={this.props.clicked}>
-                 <span data-localize='query.analyze.title'>Analyze</span>
-              </button>
-            <Overlay {...overlayProps} placement='bottom'>
-                { tooltip }
-            </Overlay>
+            <div className='table-cell table-cell-xs-4'>
+                <div className='btn-group'
+                     data-localize='query.analyze.help'
+                     ref='analyze_tooltip_target'
+                >
+                    <button className='btn btn-primary' type='button' disabled={isFetchingSamples}
+                            onClick={this.props.clicked}>
+                        <span data-localize='query.analyze.title'>Analyze</span>
+                    </button>
+                    <Overlay {...overlayProps} placement='bottom'>
+                        { tooltip }
+                    </Overlay>
+                </div>
             </div>
-
-        </div>
-
-    );
+        );
     }
 }
