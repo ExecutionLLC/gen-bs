437b2491d8618605a03f5283fb46321e55789735
dmitry <dmitry.zyryanov@execution.su>
2016-05-19 10:33:28 +0600

    issue 252, same name filters and views: check for unque view name at frontend

--- frontend/app/components/Modals/ViewsModal.js
+++ frontend/app/components/Modals/ViewsModal.js
@@ -16,6 +16,7 @@ class ViewsModal extends React.Component {
 
     render() {
         const {auth} = this.props;
+        const {views} = this.props.userData;
         const {showModal, closeModal, viewBuilder} = this.props;
         const editedView = viewBuilder.editedView;
         const isNew = editedView ? editedView.id === null : false;
@@ -24,7 +25,16 @@ class ViewsModal extends React.Component {
         const isLoginRequired = isViewAdvanced && auth.isDemo;
         const editedViewNameTrimmed = editedView && editedView.name.trim();
 
-        const validationMessage = !editedViewNameTrimmed ? 'View name cannot be empty' : '';
+        const viewNameExists = isViewEditable && _(views)
+                .filter(view => view.type !== 'history')
+                .some(view => view.name.trim() === editedViewNameTrimmed
+                    && view.id != editedView.id
+                );
+
+        const validationMessage =
+            viewNameExists ? 'View with this name is already exists.' :
+                editedView && !editedViewNameTrimmed ? 'View name cannot be empty' :
+                    '';
 
         const confirmButtonParams = {
             caption: isViewEditable ? 'Save and Select' : 'Select',
@@ -80,11 +90,12 @@ class ViewsModal extends React.Component {
 }
 
 function mapStateToProps(state) {
-    const {auth, viewBuilder} = state;
+    const {auth, viewBuilder, userData} = state;
 
     return {
         auth,
-        viewBuilder
+        viewBuilder,
+        userData
     };
 }
 
