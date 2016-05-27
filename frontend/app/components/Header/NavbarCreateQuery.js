f9fb8b87489f6116d8816c7a4494ab12b3475810
dmitry <dmitry.zyryanov@execution.su>
2016-05-27 10:32:03 +0600

    issue 353, views refactor, code review: TODO comment removed

--- frontend/app/components/Header/NavbarCreateQuery.js
+++ frontend/app/components/Header/NavbarCreateQuery.js
@@ -80,7 +80,7 @@ function mapStateToProps(state) {
         auth,
         samplesList,
         samplesList: {
-            samples // TODO why not use samplesList.samples?
+            samples
         },
         filtersList,
         viewsList
