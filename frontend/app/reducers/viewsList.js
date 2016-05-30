5636b5259da25c3a8794b6a64afea0c5b4aa4096
dmitry <dmitry.zyryanov@execution.su>
2016-05-30 11:40:10 +0600

    issue 353, views refactor, code moved to utils

--- frontend/app/reducers/viewsList.js
+++ frontend/app/reducers/viewsList.js
@@ -1,52 +1,5 @@
-import _ from 'lodash';
 import * as ActionTypes from '../actions/viewsList';
-import immutableArray from '../utils/immutableArray';
-
-
-class ImmutableHashedArray {
-    static makeFromArray(array) {
-        return {
-            array: array || [],
-            hash: _.keyBy(array, 'id')
-        };
-    }
-
-    static _findIndexForId(array, id) {
-        return _.findIndex(array, {id: id});
-    }
-
-    static deleteItemId({array, hash}, id) {
-        const itemIndex = this._findIndexForId(array, id);
-        if (itemIndex < 0) {
-            return null;
-        } else {
-            return {
-                array: immutableArray.remove(array, itemIndex),
-                hash: _.omit(hash, id)
-            };
-        }
-    }
-
-    static replaceItemId({array, hash}, id, newItem) {
-        const itemIndex = this._findIndexForId(array, id);
-        if (itemIndex < 0) {
-            return null;
-        } else {
-            return {
-                array: immutableArray.replace(array, itemIndex, newItem),
-                hash: {..._.omit(hash, id), [newItem.id]: newItem}
-            };
-        }
-    }
-
-    static appendItem({array, hash}, newItem) {
-        return {
-            array: immutableArray.append(array, newItem),
-            hash: {...hash, [newItem.id]: newItem}
-        };
-    }
-}
-
+import {ImmutableHashedArray} from '../utils/immutable';
 
 function reduceViewListDeleteView(state, action) {
     const newHashedArray = ImmutableHashedArray.deleteItemId(state.hashedArray, action.viewId);
