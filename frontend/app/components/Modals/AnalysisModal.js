6c014d712ed9212c32f9095cf695251025154fe6
dmitry <dmitry.zyryanov@execution.su>
2016-06-02 12:42:35 +0600

    analysis modal: footer removed

--- frontend/app/components/Modals/AnalysisModal.js
+++ frontend/app/components/Modals/AnalysisModal.js
@@ -17,8 +17,6 @@ class AnalysisModal extends React.Component {
                 <AnalysisHeader />
                 <Modal.Body>
                 </Modal.Body>
-                <Modal.Footer>
-                </Modal.Footer>
             </Modal>
         );
     }
