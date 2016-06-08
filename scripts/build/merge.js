c90eeac59f00615c5327f74f080b524e752fadc8
Vasily Loginov <loginov.vasily@gmail.com>
2016-06-08 15:40:29 +0600

    Fixed pg dep to resolve connection bug. Fixed express-rate-limit dep to use the normal package instead of repo link. Added webserver uglifier build.

---
+++ scripts/build/merge.js
@@ -0,0 +1,17 @@
+const NodeOptimize = require('node-optimize');
+const args = require('optimist').argv;
+const PACKAGE_ROOT = '../..';
+
+const outFilePath = args.out;
+if (!outFilePath) {
+    throw new Error('Output file path is not set, use --out.');
+}
+
+const optimizer = new NodeOptimize({
+    ignore: [
+        PACKAGE_ROOT + '/utils/Config.js'
+    ]
+});
+
+var mergedJs = optimizer.merge(PACKAGE_ROOT + '/index.js');
+require('fs').writeFile(require('path').resolve(outFilePath), mergedJs);
