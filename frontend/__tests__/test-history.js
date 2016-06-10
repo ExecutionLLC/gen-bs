26fae36b6d09270fc1b569334858b6d4c5e13800
Vasily Loginov <loginov.vasily@gmail.com>
2016-06-10 20:18:53 +0600

    Working on checking that history items are selected.

--- frontend/__tests__/test-history.js
+++ frontend/__tests__/test-history.js
@@ -26,11 +26,11 @@ function buildHistoryState() {
         filtersList: {hashedArray:{array: filters}},
         fields
     } = MOCK_APP_STATE;
-    const historyView = views[0];
-    const historyFilter = filters[0];
-    const historySample = samples[0];
+    const historyView = Object.assign({}, views[0], {id: 'historyViewId'});
+    const historyFilter = Object.assign({}, filters[0], {id: 'historyFilterId'});
+    const historySample = Object.assign({}, samples[0], {id: 'historySampleId'});
     const historyEntry = {
-        id: '005f858f-a723-4556-b45f-1464abb074f3',
+        id: 'historyEntryId',
         timestamp: '2016-05-31T10:52:17.813Z',
         view: historyView,
         filters: [historyFilter],
@@ -40,13 +40,16 @@ function buildHistoryState() {
     const initialAppState = {
         fields,
         viewsList: {
-            hashedArray: ImmutableHashedArray.makeFromArray(views.slice(1))
+            hashedArray: ImmutableHashedArray.makeFromArray(views.slice(1)),
+            selectedViewId: views[1].id
         },
         samplesList: {
-            samples: samples.slice(1)
+            samples: samples.slice(1),
+            selectedSample: samples[1]
         },
         filtersList: {
-            hashedArray: ImmutableHashedArray.makeFromArray(filters.slice(1))
+            hashedArray: ImmutableHashedArray.makeFromArray(filters.slice(1)),
+            selectedFilterId: filters[1].id
         },
         queryHistory: Object.assign({}, MOCK_APP_STATE.queryHistory, {
             history: [historyEntry]
@@ -61,23 +64,31 @@ function buildHistoryState() {
     };
 }
 
+function mockGetFields(sessionId, sampleId, expectedSampleId) {
+    expect(sampleId).toEqual(expectedSampleId);
+    return Promise.resolve(MOCK_APP_STATE.fields.sampleFieldsList);
+}
+
 function mockGetAllFields(sessionId, callback) {
-    callback(null, APP_STATE.fields.totalFieldsList);
+    callback(null, MOCK_APP_STATE.fields.totalFieldsList);
 }
 
 /**@returns {{views:Array, filters:Array, samples:Array, history:Array}}*/
 function mapStateToCollections(globalState) {
     const {
-        viewsList: {hashedArray:{array:views}},
-        filtersList: {hashedArray:{array:filters}},
-        samplesList: {samples},
+        viewsList: {hashedArray:{array:views}, selectedViewId},
+        filtersList: {hashedArray:{array:filters}, selectedFilterId},
+        samplesList: {samples, selectedSample},
         queryHistory: {history}
     } = globalState;
     return {
         views,
         filters,
         samples,
-        history
+        history,
+        selectedViewId,
+        selectedFilterId,
+        selectedSampleId: selectedSample.id
     };
 }
 
@@ -94,7 +105,10 @@ describe('Mocked History State', () => {
         historySample,
         historyEntry
     } = state;
-    const {filters, views, samples, history} = mapStateToCollections(initialAppState);
+    const {
+        filters, views, samples, history,
+        selectedFilterId, selectedViewId, selectedSampleId
+    } = mapStateToCollections(initialAppState);
 
     it('should not contain history items', () => {
         expectItemByPredicate(views, view => view.id === historyView.id).toBeFalsy();
@@ -105,34 +119,58 @@ describe('Mocked History State', () => {
     it('should contain correct history entry', () => {
         expectItemByPredicate(history, entry => entry.id === historyEntry.id).toBeTruthy();
     });
+
+    it('should have selected items, and they should not be history items', () => {
+        function checkSelectionCorrect(selectedId, historyItem) {
+            expect(selectedId).toBeTruthy();
+            expect(selectedId).not.toEqual(historyItem.id);
+        }
+
+        checkSelectionCorrect(selectedFilterId, historyFilter);
+        checkSelectionCorrect(selectedViewId, historyView);
+        checkSelectionCorrect(selectedSampleId, historySample);
+    })
 });
 
 describe('History Tests', () => {
+    const {
+        initialAppState,
+        historyView,
+        historyFilter,
+        historySample,
+        historyEntry
+    } = buildHistoryState();
+
     beforeEach(() => {
+        apiFacade.samplesClient.getFields = (sessionId, sampleId) => mockGetFields(sessionId, sampleId, historySample.id);
         apiFacade.samplesClient.getAllFields = mockGetAllFields;
     });
 
     afterEach(() => {
+        delete apiFacade.samplesClient.getFields;
         delete apiFacade.samplesClient.getAllFields;
     });
     
     it('should correctly renew history item', (done) => {
-        const {
-            initialAppState,
-            historyView,
-            historyFilter,
-            historySample,
-            historyEntry
-        } = buildHistoryState();
         storeTestUtils.runTest({
             globalInitialState: initialAppState,
             applyActions: (dispatch) => dispatch(renewHistoryItem(historyEntry.id))
         }, (globalState) => {
-            const {views, samples, filters, history} = mapStateToCollections(globalState);
+            const {
+                views, samples, filters, history,
+                selectedFilterId, selectedViewId, selectedSampleId
+            } = mapStateToCollections(globalState);
+
+            // History items should be in collections.
             expectItemByPredicate(history, item => item.id === historyEntry.id).toBeTruthy();
             expectItemByPredicate(filters, item => item.id === historyFilter.id).toBeTruthy();
             expectItemByPredicate(views, item => item.id === historyView.id).toBeTruthy();
             expectItemByPredicate(samples, item => item.id === historySample.id).toBeTruthy();
+
+            // History items should be selected in lists.
+            expect(selectedFilterId).toEqual(historyFilter.id);
+            expect(selectedViewId).toEqual(historyView.id);
+            expect(selectedSampleId).toEqual(historySample.id);
 
             done();
         });
