5b9c3525169e00b3b16ed5ad9118a1a68a45112d
dmitry <dmitry.zyryanov@execution.su>
2016-06-10 14:33:59 +0600

    jest, fields testing: show expected and actual states difference

--- frontend/__tests__/storeTestUtils.js
+++ frontend/__tests__/storeTestUtils.js
@@ -8,6 +8,99 @@ import configureStore from '../app/store/configureStore';
  * @property {function()}getState
  * */
 
+
+var deepDiffMapper = function() {
+    return {
+        VALUE_CREATED: 'created',
+        VALUE_UPDATED: 'updated',
+        VALUE_DELETED: 'deleted',
+        VALUE_UNCHANGED: 'unchanged',
+        map: function(obj1, obj2) {
+            if (this.isFunction(obj1) || this.isFunction(obj2)) {
+                throw 'Invalid argument. Function given, object expected.';
+            }
+            if (this.isValue(obj1) || this.isValue(obj2)) {
+                var cv = this.compareValues(obj1, obj2);
+                var cvchar = {[this.VALUE_UNCHANGED]: '!', [this.VALUE_CREATED]: '+', [this.VALUE_DELETED]: '-', [this.VALUE_UPDATED]: '~'}[cv];
+                if (typeof cvchar === 'undefined') cvchar = '?';
+                return cvchar;
+                //return {type: this.compareValues(obj1, obj2), data: obj1 || obj2};
+            }
+
+            var diff = {};
+            var diff2 = [];
+            for (var key in obj1) {
+                if (this.isFunction(obj1[key])) {
+                    continue;
+                }
+
+                var value2 = undefined;
+                if ('undefined' != typeof(obj2[key])) {
+                    value2 = obj2[key];
+                }
+
+                diff[key] = this.map(obj1[key], value2);
+                if (typeof diff[key] == 'string') {
+                    if (diff[key] != '!') {
+                        diff2.push(key + diff[key]);
+                    }
+                }
+                else {
+                    if (!this.isArray(diff[key]) || diff[key].length) {
+                        diff2.push({[key]:  diff[key]});
+                    }
+                }
+            }
+            for (var key in obj2) {
+                if (this.isFunction(obj2[key]) || ('undefined' != typeof(diff[key]))) {
+                    continue;
+                }
+
+                diff[key] = this.map(undefined, obj2[key]);
+                if (typeof diff[key] == 'string') {
+                    if (diff[key] != '!') {
+                        diff2.push(key + diff[key]);
+                    }
+                }
+                else {
+                    if (!this.isArray(diff[key]) || diff[key].length) {
+                        diff2.push({[key]:  diff[key]});
+                    }
+                }
+            }
+
+            return diff2;
+
+        },
+        compareValues: function(value1, value2) {
+            if (value1 === value2) {
+                return this.VALUE_UNCHANGED;
+            }
+            if ('undefined' == typeof(value1)) {
+                return this.VALUE_CREATED;
+            }
+            if ('undefined' == typeof(value2)) {
+                return this.VALUE_DELETED;
+            }
+
+            return this.VALUE_UPDATED;
+        },
+        isFunction: function(obj) {
+            return {}.toString.apply(obj) === '[object Function]';
+        },
+        isArray: function(obj) {
+            return {}.toString.apply(obj) === '[object Array]';
+        },
+        isObject: function(obj) {
+            return {}.toString.apply(obj) === '[object Object]';
+        },
+        isValue: function(obj) {
+            return !this.isObject(obj) && !this.isArray(obj);
+        }
+    }
+}();
+
+
 /**
  * @typedef {Object}TestCase
  * @property {string|undefined}name
@@ -90,9 +183,11 @@ export default class StoreTestUtils {
      * @param {Object}state
      * */
     static _gracefulFail(test, state) {
-        const difference = _.differenceWith(state, test.expectedState, _.isEqual);
+        const difference = deepDiffMapper.map(test.expectedState, state);
         const testNameString = test.name ? `[${test.name}] ` : '';
-        throw new Error(`${testNameString}States are different: ${JSON.stringify(difference, null, 2)}, `
-            + `\nstate: ${JSON.stringify(state, null, 2)}`);
+        throw new Error(`${testNameString}States are different: ${JSON.stringify(test.expectedState, null, 2)}, `
+            + `\n\nstate: ${JSON.stringify(state, null, 2)}`
+            + `\n\ndiff: ${JSON.stringify(difference, null, 2)}`
+        );
     }
 }
