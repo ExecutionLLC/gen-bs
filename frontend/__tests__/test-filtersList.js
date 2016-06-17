863f30f99aaa1bdaf58c6728e745b04b6f010727
dmitry <dmitry.zyryanov@execution.su>
2016-06-17 18:12:52 +0600

    jest, filters testing: create tests

--- frontend/__tests__/test-filtersList.js
+++ frontend/__tests__/test-filtersList.js
@@ -13,11 +13,13 @@ import {filtersListServerCreateFilter, filtersListServerUpdateFilter, filtersLis
 function buildFiltersState(appState) {
     const {
         auth,
+        ui,
         filtersList: {hashedArray: {array: filters}}
     } = appState;
 
     const initialAppState = {
         auth: auth,
+        ui: ui,
         filtersList: {
             hashedArray: ImmutableHashedArray.makeFromArray(filters),
             selectedFilterId: filters[0].id
@@ -55,6 +57,17 @@ function mockFilterRemove(sessionId, filterId, callback, expected) {
 function mockFilterUpdate(sessionId, filter, callback, expected) {
     expect(filter).toEqual(expected.filter);
     expect(sessionId).toEqual(expected.sessionId);
+    if (expected.error) {
+        return callback(expected.error, {status: 500});
+    } else {
+        return callback(null, {status: HttpStatus.OK, body: expected.filterResponse});
+    }
+}
+
+function mockFilterCreate(sessionId, languageId, filter, callback, expected) {
+    expect(filter).toEqual(expected.filter);
+    expect(sessionId).toEqual(expected.sessionId);
+    expect(languageId).toEqual(expected.languageId);
     if (expected.error) {
         return callback(expected.error, {status: 500});
     } else {
@@ -309,4 +322,113 @@ describe('Filters list update tests', () => {
 
     runTests('run updating success', updateTestsItsMocksArraySuccess);
     runTests('run updating error', updateTestsItsMocksArrayError);
+});
+
+describe('Filters list create tests', () => {
+    const {initialAppState, filters} = buildFiltersState(MOCK_APP_STATE);
+    const {sessionId} = initialAppState.auth;
+    const languageId = initialAppState.ui.language;
+
+    const newFilter = {
+        "id": null,
+        "originalFilterId": null,
+        "name": "Updated Filter",
+        "rules": {
+            "$and": [
+                {
+                    "00000000-0000-0000-0000-000000000007": {
+                        "$eq": "SAPP"
+                    }
+                },
+                {
+                    "00000000-0000-0000-0000-000000000005": {
+                        "$neq": "B"
+                    }
+                },
+                {
+                    "$or": [
+                        {
+                            "69d1d2db-1d7b-4a9e-a3ee-d5108da78c84": {
+                                "$eq": "CBA"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+
+    const testCases = [
+        {description: 'should create filter', newFilter: newFilter}
+    ];
+
+    function makeTests(describeName, testCases, mustError) {
+
+        const tests = testCases.map((testCase) => {
+            const {description, newFilter} = testCase;
+            const newFilterId = '' + Math.random();
+            const filterToResponse = {..._.cloneDeep(newFilter), id: newFilterId};
+            const initialFiltersHashedArray = ImmutableHashedArray.makeFromArray(filters);
+            const expectedFiltersHashedArray = mustError ?
+                initialFiltersHashedArray :
+                ImmutableHashedArray.appendItem(initialFiltersHashedArray, {...newFilter, id: newFilterId});
+            
+            function actions(dispatch) {
+                return dispatch(filtersListServerCreateFilter(newFilter, sessionId, languageId));
+            }
+
+            function checkState(globalState) {
+                //expect(123).toBe(456);
+                const {filtersList: {hashedArray: filtersHashedArray}} = globalState;
+                checkHashedArraysEqual(filtersHashedArray, expectedFiltersHashedArray);
+            }
+
+            function setMocks() {
+                apiFacade.filtersClient.add = (sessionId, languageId, requestFilter, callback) => mockFilterCreate(
+                    sessionId, languageId, requestFilter, callback,
+                    {sessionId: sessionId, languageId: languageId, filter: newFilter, filterResponse: filterToResponse, error: mustError ? {message: 'mockError'} : null}
+                );
+            }
+
+            return {
+                it: () => {
+                    it(description, (done) => {
+                        storeTestUtils.runTest({
+                            globalInitialState: initialAppState,
+                            applyActions: actions
+                        }, (globalState) => {
+                            checkState(globalState);
+                            done();
+                        });
+                    })
+                },
+                setMocks: setMocks
+            };
+        });
+
+        function resetMocks() {
+            delete apiFacade.filtersClient.add;
+        }
+
+        return () => {
+            describe(describeName, () => {
+                var testIndex = 0;
+
+                beforeEach(() => {
+                    tests[testIndex++].setMocks();
+                });
+
+                afterEach(() => {
+                    resetMocks();
+                });
+
+                tests.forEach((test) => test.it());
+            })
+        };
+    }
+
+    const testsSuccess = makeTests('run creating success', testCases, false);
+    const testsError = makeTests('run creating error', testCases, true);
+    testsSuccess();
+    testsError();
 });
