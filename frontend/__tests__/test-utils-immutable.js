9994c35c0e8454bdca1a23171599dec38722576f
dmitry <dmitry.zyryanov@execution.su>
2016-06-22 10:51:11 +0600

    jest, immutable functions test: checked expected behavior only

--- frontend/__tests__/test-utils-immutable.js
+++ frontend/__tests__/test-utils-immutable.js
@@ -4,20 +4,17 @@ import {ImmutableHashedArray} from '../app/utils/immutable';
 describe('Immutable array', () => {
     it('should replace item', () => {
         expect(() => immutableArray.replace(null, 3, 123)).toThrow();
-        // unexpected behavior when index out of range
-        expect(immutableArray.replace([], 3, 123)).toEqual([123]);
-        // unexpected behavior when index is negative
-        expect(immutableArray.replace([11, 22, 33], -1, 123)).toEqual([11, 22, 123, 11, 22, 33]);
+        expect(immutableArray.replace([], 3, 123)).toEqual([]);
+        expect(immutableArray.replace([11, 22, 33], -1, 123)).toEqual([11, 22, 33]);
         expect(immutableArray.replace([11, 22, 33], 0, 123)).toEqual([123, 22, 33]);
         expect(immutableArray.replace([11, 22, 33], 1, 123)).toEqual([11, 123, 33]);
         expect(immutableArray.replace([11, 22, 33], 2, 123)).toEqual([11, 22, 123]);
-        expect(immutableArray.replace([11, 22, 33], 4, 123)).toEqual([11, 22, 33, 123]);
+        expect(immutableArray.replace([11, 22, 33], 4, 123)).toEqual([11, 22, 33]);
     });
     it('should remove item', () => {
         expect(() => immutableArray.remove(null, 3)).toThrow();
         expect(immutableArray.remove([], 3)).toEqual([]);
-        // unexpected behavior when index is negative
-        expect(immutableArray.remove([11, 22, 33], -1)).toEqual([11, 22, 11, 22, 33]);
+        expect(immutableArray.remove([11, 22, 33], -1)).toEqual([11, 22, 33]);
         expect(immutableArray.remove([11, 22, 33], 0)).toEqual([22, 33]);
         expect(immutableArray.remove([11, 22, 33], 1)).toEqual([11, 33]);
         expect(immutableArray.remove([11, 22, 33], 2)).toEqual([11, 22]);
