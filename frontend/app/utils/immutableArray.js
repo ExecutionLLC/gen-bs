683ca7fb48c0b84de295cb289e653b609fb99400
dmitry <dmitry.zyryanov@execution.su>
2016-06-22 17:33:30 +0600

    immutable utils fixes: throw when replace wrong index

--- frontend/app/utils/immutableArray.js
+++ frontend/app/utils/immutableArray.js
@@ -7,6 +7,9 @@ export default class immutableArray {
      * @returns {*}
      */
     static replace(arr, index, item) {
+        if (index < 0 || index > arr.length) {
+            throw 'immutableArray.replace wrong index = ' + index + ', arr.length = ' + arr.length;
+        }
         return [
             ...arr.slice(0, index),
             item,
