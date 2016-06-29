af0f510a42d919b57bbb0f6261c6b0f3e68d40e7
Vasily Loginov <loginov.vasily@gmail.com>
2016-06-29 15:32:58 +0600

    Now env is passing successfully to the production container. Put uploads and logs to volume.

--- docker/scripts/docker-run.sh
+++ docker/scripts/docker-run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+set -e
+set -x
+
 WS_ROOT=/webserver
 FRONTEND_JS_FILE=genomics.js
 FRONTEND_JS_TEMPLATE=${FRONTEND_JS_FILE}.template
 WS_SCRIPT_PATH=${WS_ROOT}/webserver.js
+
+
+echo "Log file path: ${GEN_WS_LOG_PATH}"
+echo "Samples upload path: ${GEN_WS_UPLOAD_PATH}"
+echo "Saved files upload path: ${GEN_WS_SAVED_FILES_PATH}"
+
+LOG_DIR=$(dirname ${GEN_WS_LOG_PATH})
+mkdir -p ${LOG_DIR} ${GEN_WS_UPLOAD_PATH} ${GEN_WS_SAVED_FILES_PATH}
 
 # Load NVM
 source ${HOME}/.profile
@@ -14,8 +25,8 @@ FRONTEND_ENV_VARS=$(env | grep GEN_FRONTEND)
 SED_REGEX=''
 
 while read -r ENV_VAR; do
-    ENV_VAR_NAME="${ENV_VAR#*=}"
-    ENV_VAR_VALUE="${ENV_VAR%*=}"
+    ENV_VAR_NAME="$(echo ${ENV_VAR} | cut -d '=' -f 1)"
+    ENV_VAR_VALUE="$(echo ${ENV_VAR} | cut -d '=' -f 2)"
 
     ENV_VAR_TEMPLATE="##${ENV_VAR_NAME}##"
     ENV_VAR_REGEX="s/${ENV_VAR_TEMPLATE}/${ENV_VAR_VALUE}/g"
@@ -25,8 +36,5 @@ done <<< "${FRONTEND_ENV_VARS}"
 
 # Build actual JS file.
 cat ${WS_ROOT}/public/${FRONTEND_JS_TEMPLATE} | sed "${SED_REGEX}" > ${WS_ROOT}/public/${FRONTEND_JS_FILE}
-
-GEN_WS_LOG_PATH=${WS_ROOT}/logs/webserver.log
-mkdir -p ${GEN_WS_LOG_PATH}
 
 node ${WS_SCRIPT_PATH}
