d8d5d16fb91194fef0d2f5257cf313050f7c6846
Vasily Loginov <loginov.vasily@gmail.com>
2016-07-01 19:29:55 +0600

    Reformatted file.

--- services/ViewsService.js
+++ services/ViewsService.js
@@ -6,47 +6,47 @@ const async = require('async');
 const UserEntityServiceBase = require('./UserEntityServiceBase');
 
 class ViewsService extends UserEntityServiceBase {
-  constructor(services, models) {
-      super(services, models, models.views);
-  }
+    constructor(services, models) {
+        super(services, models, models.views);
+    }
 
-  add(user, languId, item, callback) {
-    async.waterfall([
-        (callback) => {
-            this._viewItemsCheck(item, callback);
-        },
-        (item, callback) => super.findAll(user, callback),
-        (views, callback) => {
-            this._checkViewNameExists(item, views, callback);
-        },
-        (item, callback) => {
-            super.add(user, languId, item, callback);
+    add(user, languId, item, callback) {
+        async.waterfall([
+            (callback) => {
+                this._viewItemsCheck(item, callback);
+            },
+            (item, callback) => super.findAll(user, callback),
+            (views, callback) => {
+                this._checkViewNameExists(item, views, callback);
+            },
+            (item, callback) => {
+                super.add(user, languId, item, callback);
+            }
+        ], callback);
+    }
+
+    update(user, view, callback) {
+        async.waterfall([
+            (callback) => {
+                this._viewItemsCheck(view, callback);
+            },
+            (view, callback) => super.find(user, view.id, callback),
+            (existingView, callback) => this._ensureItemOfUserType(existingView, callback),
+            (existingView, callback) => super.update(user, view, callback)
+        ], callback);
+    }
+
+    _viewItemsCheck(view, callback) {
+        if (!view.viewListItems) {
+            callback(new Error('View list items must be defined'))
+        } else if (!_.isArray(view.viewListItems)) {
+            callback(new Error('View list items must be an array'));
+        } else if (view.viewListItems.length === 0) {
+            callback(new Error('View list items must contain at least one element'));
+        } else {
+            callback(null, view);
         }
-    ], callback);
-  }
-
-  update(user, view, callback) {
-      async.waterfall([
-          (callback) => {
-              this._viewItemsCheck(view, callback);
-          },
-          (view, callback) => super.find(user, view.id, callback),
-          (existingView, callback) => this._ensureItemOfUserType(existingView, callback),
-          (existingView, callback) => super.update(user, view, callback)
-      ], callback);
-  }
-
-  _viewItemsCheck(view, callback) {
-      if (!view.viewListItems) {
-          callback(new Error('View list items must be defined'))
-      } else if (!_.isArray(view.viewListItems)) {
-          callback(new Error('View list items must be an array'));
-      } else if (view.viewListItems.length === 0) {
-          callback(new Error('View list items must contain at least one element'));
-      } else {
-        callback(null, view);
-      }
-  }
+    }
 
     _checkViewNameExists(view, views, callback) {
         if (!_.isString(view.name)) {
