80ba0aaa19b7ed956f6da03245414f13ab3e7a56
Vasily Loginov <loginov.vasily@gmail.com>
2016-07-29 18:52:39 +0700

    Fixed progress calculation.

--- frontend/app/components/Modals/FileUpload/FileUploadProgressBar.js
+++ frontend/app/components/Modals/FileUpload/FileUploadProgressBar.js
@@ -16,18 +16,17 @@ export default class FileUploadProgressBar extends Component {
         const { progressStatus, progressValue } = this.props;
 
         const STAGES = {
-            TOTAL: 1,
+            TOTAL: 2,
             'ajax': 0,
             'task_running': 1
         };
 
         const currentStage = STAGES[progressStatus];
-        if (currentStage == null) {
+        if (!currentStage && currentStage !== 0) {
             return null;
         }
-        const currentProgress = currentStage < STAGES.TOTAL ? progressValue : 0;
         return this.renderBar('Your file is being processed...',
-            Math.round(100 * ((currentStage + currentProgress / 100) / STAGES.TOTAL))
+            Math.round((currentStage * 100 + progressValue) / STAGES.TOTAL)
         );
     }
 }
