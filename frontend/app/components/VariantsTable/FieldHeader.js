0836471c48288d588dd320db49be8808e1298e1a
dmitry <dmitry.zyryanov@execution.su>
2016-08-29 10:38:45 +0700

    show result field header with source name

--- frontend/app/components/VariantsTable/FieldHeader.js
+++ frontend/app/components/VariantsTable/FieldHeader.js
@@ -56,8 +56,8 @@ export default class FieldHeaderControls extends Component {
                 key={fieldMetadata.id}>
                 <div>
                     <div className='variants-table-header-label'>
-                        <a type='button' className='btn-link-default'>
-                            {label}
+                        <a type='button' className='btn-link-default' title={fieldMetadata.sourceName && fieldMetadata.sourceName !== 'sample' ? fieldMetadata.sourceName : null}>
+                            {label}{fieldMetadata.sourceName && fieldMetadata.sourceName !== 'sample' ? ('/' + fieldMetadata.sourceName) : null}
                         </a>
                         <div className={buttonGroupClasses}>
                             {this.renderSortButton('asc', currentDirection, ascSortBtnClasses, order, disabled)}
