4d082ee1e4b61195afd02f24b7651abfe57aee7e
Vasily Loginov <loginov.vasily@gmail.com>
2016-08-30 18:46:52 +0700

    Now save only params we need instead of JSON message.

--- database/migrations/20160819160359_upload-history.js
+++ database/migrations/20160819160359_upload-history.js
@@ -1,3 +1,5 @@
+const {SAMPLE_UPLOAD_STATUS} = require('../../utils/Enums');
+
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('sample_upload_history', (table) => {
         table.uuid('id')
@@ -6,17 +8,24 @@ exports.up = function (knex, Promise) {
             .notNullable()
             .references('id')
             .inTable('user');
-        // Sample name should not be constrained by the 'vcf_file_sample' table,
-        // as the sample is not yet created.
+        // Sample id should not be constrained by the 'vcf_file_sample' table,
+        // as the corresponding sample entry is not yet created.
         table.uuid('sample_id')
             .notNullable();
         table.string('file_name', 256)
             .notNullable();
-        table.boolean('is_active')
-            .defaultTo(true);
+        table.enu('status', [
+            SAMPLE_UPLOAD_STATUS.IN_PROGRESS,
+            SAMPLE_UPLOAD_STATUS.READY,
+            SAMPLE_UPLOAD_STATUS.ERROR
+        ])
+            .notNullable();
+        table.integer('progress')
+            .notNullable();
+        table.json('error')
+            .defaultTo(null);
         table.boolean('is_deleted')
             .defaultTo(false);
-        table.json('last_status_message');
         table.timestamp('created')
             .defaultTo(knex.fn.now());
     });
