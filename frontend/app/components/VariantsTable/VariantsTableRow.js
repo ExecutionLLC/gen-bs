fd0da76cb37c70e4b724b315cdbe9f43898feaff
dmitry <dmitry.zyryanov@execution.su>
2016-08-31 14:39:50 +0700

    merge fix

--- frontend/app/components/VariantsTable/VariantsTableRow.js
+++ frontend/app/components/VariantsTable/VariantsTableRow.js
@@ -5,7 +5,6 @@ import classNames from 'classnames';
 import ComponentBase from '../shared/ComponentBase';
 
 import VariantsTableComment from './VariantsTableComment';
-import * as _ from 'lodash';
 
 
 export default class VariantsTableRow extends ComponentBase {
