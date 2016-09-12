6dcfbe638d99785d840638f407d3aeee26f5b19f
Vasily Loginov <loginov.vasily@gmail.com>
2016-09-12 19:19:43 +0700

    Updated service stub to include additional packages.

--- registration/index.js
+++ registration/index.js
@@ -1,1 +1,23 @@
 const Express = require('express');
+const cors = require('cors');
+const compression = require('compression');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = new Express();
+app.disable('x-powered-by');
+app.use(compression());
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.post('/register', (request, response) => {
+    response.send('Hello world!');
+});
+
+app.get('/register', (request, response) => {
+    response.send('Got hello!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is started!');
+});
