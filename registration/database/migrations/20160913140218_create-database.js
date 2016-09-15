bb665088189a91680d14c05efc7a341a5f2d6b89
dmitry <dmitry.zyryanov@execution.su>
2016-09-15 15:27:45 +0700

    registration codes service: db table make migration

--- registration/database/migrations/20160913140218_create-database.js
+++ registration/database/migrations/20160913140218_create-database.js
@@ -1,8 +1,19 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('registration_code', (table) => {
+        table.uuid('id')
+            .primary();
+        table.text('description');
+        table.string('speciality');
+        table.string('language');
+        table.integer('number_of_paid_samples');
+        table.string('email', 256)
+            .defaultTo(null);
+        table.boolean('is_activated');
+        table.timestamp('activated_timestamp')
+            .defaultTo(knex.fn.now());
+    })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('registration_code');
 };
