b35dddf9b58518f618599a063e2d0213bc8c7943
dmitry <dmitry.zyryanov@execution.su>
2016-09-19 16:27:51 +0700

    registration codes service: removed unused methods from ModelBase

--- registration/models/ModelBase.js
+++ registration/models/ModelBase.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const _ = require('lodash');
-const async = require('async'); // TODO replace by Promise
-
-const Uuid = require('node-uuid');
 
 const ChangeCaseUtil = require('../utils/ChangeCaseUtil');
 const CollectionUtils = require('../utils/CollectionUtils');
@@ -22,129 +19,12 @@ class ModelBase {
         this.mappedColumns = mappedColumns;
     }
 
-    add(item, languId, callback) {
-        async.waterfall([
-            (callback) => this._add(item, languId, true, callback),
-            (itemId, callback) => this.find(itemId, callback)
-        ], callback);
-    }
-
-    addWithId(item, languId, callback) {
-        async.waterfall([
-            (callback) => this._add(item, languId, false, callback),
-            (itemId, callback) => this.find(itemId, callback)
-        ], callback);
-    }
-
-    exists(itemId, callback) {
-        this.db.asCallback((knex, callback) => {
-            knex.select('id')
-                .from(this.baseTableName)
-                .where('id', itemId)
-                .asCallback((error, itemData) => {
-                    if (error) {
-                        callback(error);
-                    } else {
-                        callback(null, (itemData.length > 0));
-                    }
-                });
-        }, callback);
-    }
-
-    find(itemId, callback) {
-        async.waterfall([
-            (callback) => this._fetch(itemId, callback),
-            (itemData, callback) => {
-                callback(null, this._mapColumns(itemData));
-            }
-        ], callback);
-    }
-
-    _add(item, languId, shouldGenerateId, callback) {
-        throw new Error('Method is abstract.');
-    }
-
-    _generateId() {
-        // Generate random UUID
-        return Uuid.v4();
-    }
-
     _mapColumns(item) {
         const itemData = ChangeCaseUtil.convertKeysToCamelCase(item);
         return CollectionUtils.createHash(this.mappedColumns,
             _.identity,
             (column) => itemData[column]
         );
-    }
-
-    /**
-     * @protected
-     * */
-    _toCamelCase(itemOrItems, callback) {
-        callback(null, ChangeCaseUtil.convertKeysToCamelCase(itemOrItems));
-    }
-
-    /**
-     * @param {string}name
-     * @param {function(Error)}callback
-     */
-    _ensureNameIsValid(name, callback) {
-        const trimmedName = (name || '').trim();
-        if (_.isEmpty(trimmedName)) {
-            callback(new Error('Name cannot be empty.'));
-        } else {
-            callback(null);
-        }
-    }
-
-    _ensureAllItemsFound(itemsFound, itemIdsToFind, callback) {
-        if (itemsFound && itemsFound.length === itemIdsToFind.length) {
-            callback(null, itemsFound);
-        } else {
-            callback('Part of the items is not found: ' + itemIdsToFind);
-        }
-    }
-
-    _mapItems(items, callback) {
-        async.map(items, (item, callback) => {
-            callback(null, this._mapColumns(item));
-        }, callback);
-    }
-
-    _fetch(itemId, callback) { // TODO return Promise
-        this.db.asCallback((knex, callback) => {
-            knex.select()
-                .from(this.baseTableName)
-                .where('id', itemId)
-                .asCallback((error, itemData) => {
-                    if (error || !itemData.length) {
-                        callback(error || new Error('Item not found: ' + itemId));
-                    } else {
-                        callback(null, ChangeCaseUtil.convertKeysToCamelCase(itemData[0]));
-                    }
-                });
-        }, callback);
-    }
-
-    _insert(dataToInsert, trx, callback) {
-        this._unsafeInsert(this.baseTableName, dataToInsert, trx, callback);
-    }
-
-    _unsafeInsert(tableName, dataToInsert, trx, callback) {
-        trx(tableName)
-            .insert(ChangeCaseUtil.convertKeysToSnakeCase(dataToInsert))
-            .asCallback((error) => {
-                callback(error, dataToInsert.id);
-            });
-    }
-
-    _unsafeUpdate(itemId, dataToUpdate, trx, callback) {
-        trx(this.baseTableName)
-            .where('id', itemId)
-            .update(ChangeCaseUtil.convertKeysToSnakeCase(dataToUpdate))
-            .asCallback((error) => {
-                callback(error, itemId);
-            });
     }
 }
 
