ded01c6e451059827d96164a39b759d13ba4910a
dmitry <dmitry.zyryanov@execution.su>
2016-09-19 16:33:40 +0700

    registration codes service: jsdoc

--- registration/models/ModelBase.js
+++ registration/models/ModelBase.js
@@ -6,11 +6,18 @@ const ChangeCaseUtil = require('../utils/ChangeCaseUtil');
 const CollectionUtils = require('../utils/CollectionUtils');
 
 class ModelBase {
+    /**@typedef {Object} ModelBase
+     * @property {KnexWrapper} db
+     * @property {Logger} logger
+     * @property {string} baseTableName
+     * @property {string[]} mappedColumns
+     **/
+
     /**
-     * @param db
+     * @param {KnexWrapper} db
      * @param {Logger} logger
-     * @param baseTableName Name of the main (or the only) table of the corresponding model.
-     * @param mappedColumns List of column names that will be allowed to extract from the table(s) (@see ModelBase._mapColumns() method).
+     * @param {string} baseTableName Name of the main (or the only) table of the corresponding model.
+     * @param {string[]} mappedColumns List of column names that will be allowed to extract from the table(s) (@see ModelBase._mapColumns() method).
      * */
     constructor(db, logger, baseTableName, mappedColumns) {
         this.db = db;
