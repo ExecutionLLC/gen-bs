62f4cb389226a4946dc72122f4f5b71971f638e4
dmitry <dmitry.zyryanov@execution.su>
2016-09-20 10:53:36 +0700

    registration codes service: do activation via request

--- registration/index.js
+++ registration/index.js
@@ -4,6 +4,21 @@ const compression = require('compression');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const Config = require('./Config');
+const Logger = require('./utils/Logger');
+const logger = new Logger(Config.logger);
+
+const KnexWrapper = require('./utils/KnexWrapper');
+const RegistrationCodesService = require('./services/RegistrationCodesService');
+const RegistrationCodesModel = require('./models/RegistrationCodesModel');
+const UsersClient = require('./api/UsersClient');
+
+const dbModel = new KnexWrapper(Config, logger);
+const registrationCodesModel = new RegistrationCodesModel(dbModel, logger);
+const usersClient = new UsersClient(Config);
+
+const registrationCodes = new RegistrationCodesService(dbModel, registrationCodesModel, usersClient);
+
 const app = new Express();
 app.disable('x-powered-by');
 app.use(compression());
@@ -11,7 +26,16 @@ app.use(bodyParser.json());
 app.use(morgan('combined'));
 
 app.post('/register', (request, response) => {
-    response.send('Hello world!');
+    console.log('POSTed');
+    console.log(request.body.id);
+    console.log(request.body);
+    registrationCodes.activateAsync()
+        .then(() =>
+            response.send({success: true})
+        )
+        .catch((error) =>
+            response.send({success: false, error})
+        );
 });
 
 app.get('/register', (request, response) => {
