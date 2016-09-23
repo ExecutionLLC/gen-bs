0de85c7b0a4631f145d1e19d9935901147660289
Vasily Loginov <loginov.vasily@gmail.com>
2016-09-23 18:02:35 +0700

    Added migration script.

---
+++ registration/database/scripts/migrate.js
@@ -0,0 +1,67 @@
+'use strict';
+const knex = require('knex');
+const knexfile = require('./../knexfile');
+
+// It is impossible to create database during migrations,
+// therefore use knex migration API directly here.
+
+const env = process.env.NODE_ENV || 'development';
+const {connection: connectionSettings, client} = knexfile[env];
+const {database, user, password} = connectionSettings;
+
+function createConfigForDatabaseName(databaseName) {
+    return {
+        client,
+        connection: {
+            user,
+            password,
+            database: databaseName
+        }
+    };
+}
+
+const postgresKnex = knex(createConfigForDatabaseName('postgres'));
+Promise.resolve()
+    .then(() => {
+        // Check if database exists.
+        return postgresKnex('pg_database')
+            .select()
+            .where({
+                datname: database
+            })
+            .then((databases) => databases.length > 0)
+    })
+    .then((databaseExists) => {
+        if (!databaseExists) {
+            console.log(`Database ${database} doesn't exist, create...`);
+            return postgresKnex.raw(`CREATE DATABASE ${database}`)
+                .then(() => {
+                    console.log(`Database ${database} is created.`);
+                    return true;
+                });
+        } else {
+            console.log(`Database ${database} exists.`);
+            return false;
+        }
+    })
+    .then(() => {
+        console.log('Destroying "postgres" database context.');
+        return postgresKnex.destroy();
+    })
+    .then(() => knex(createConfigForDatabaseName(database)))
+    .then((knex) => {
+        return knex.migrate.latest({directory: __dirname + '/../migrations'})
+            .then(() => knex);
+    })
+    .then((knex) => {
+        console.log('Destroying database context');
+        return knex.destroy();
+    })
+    .then(() => {
+        console.log('Completed!');
+        process.exit(0);
+    })
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
