3c68bdbc6f92e9910bfc160c8fdcc79a860f4527
ponomarevvl90 <ponomarev.vl90@gmail.com>
2016-09-23 12:10:46 +0700

    fix double qoutes

--- frontend/app/components/Header/NavbarMain/LoginForm.js
+++ frontend/app/components/Header/NavbarMain/LoginForm.js
@@ -15,19 +15,19 @@ class LoginForm extends Component {
         const isDisable = login && password ? false : true;
         return (
             <div className='login-form'>
-                <input className="form-control"
+                <input className='form-control'
                        value={login}
                        type='text'
                        key='login'
                        placeholder='Login'
                        onChange={(e) => this.onLoginChanged(e)}/>
-                <input className="form-control"
+                <input className='form-control'
                        value={password}
                        type='password'
                        key='password'
                        placeholder='Password'
                        onChange={(e) => this.onPasswordChanged(e)}/>
-                <button className="btn btn-sm btn-primary login-button"
+                <button className='btn btn-sm btn-primary login-button'
                         type='button'
                         onClick={(e) => this.onLoginClick(e)}
                         disabled={isDisable}
