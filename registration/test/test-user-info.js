6fb9753a316c4a7f71262dc45cd2faf09b208454
dmitry <dmitry.zyryanov@execution.su>
2016-09-26 19:57:19 +0700

    registration codes service form: unused functions tests removed

--- registration/test/test-user-info.js
+++ registration/test/test-user-info.js
@@ -25,23 +25,6 @@ describe('User info', () => {
     }
 
     describe('Positive tests', () => {
-        it('should add user and find it for regcode', () => {
-            const userRegcode = generateRegcode();
-            const userFirstName = userRegcode + '-first-name';
-            return userInfo.create({regcode: userRegcode, firstName: userFirstName})
-                .then((user) => {
-                    assert.ok(user, `Not created user ${({regcode: userRegcode, firstName: userFirstName})}`);
-                    return user.id;
-                })
-                .then((userId) =>
-                    userInfo.findByRegcodeOrEmailAsync(userRegcode) // TODO remove test
-                        .then((foundUser) => {
-                            assert.equal(foundUser.id, userId, 'found created user id');
-                            assert.equal(foundUser.firstName, userFirstName, 'found created user name');
-                            assert.equal(foundUser.regcode, userRegcode, 'found created user regcode');
-                        })
-                )
-        });
         it('should update found user', () => {
             const userRegcode = generateRegcode();
             const userEmail = generateEmail();
@@ -61,27 +44,11 @@ describe('User info', () => {
     });
 
     describe('Negative tests', () => {
-        it('should not find absent user', () =>
-            Promise.resolve()
-                .then(() => mustThrowPromise(
-                    userInfo.findByRegcodeOrEmailAsync(generateRegcode()), // TODO remove test
-                    'find for absent regcode'
-                ))
-        );
         it('shoult not add user without email or regcode', () =>
             Promise.resolve()
                 .then(() => mustThrowPromise(
                     userInfo.create({}),
                     'add user with no email and regcode'
-                ))
-        );
-        it('should not find user with no regcode', () =>
-            Promise.resolve()
-                .then(() => userInfo.create({email: generateEmail()}))
-                .then(() => userInfo.create({regcode: generateRegcode()}))
-                .then(() => mustThrowPromise(
-                    userInfo.findByRegcodeOrEmailAsync(null), // TODO remove test
-                    'find for null regcode'
                 ))
         );
     });
