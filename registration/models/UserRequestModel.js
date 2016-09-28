3affb7f0f63d7aab525a716e730d7887a30df446
dmitry <dmitry.zyryanov@execution.su>
2016-09-28 16:10:28 +0700

    registration codes service form: add user request table

---
+++ registration/models/UserRequestModel.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Uuid = require('node-uuid');
+const Promise = require('bluebird');
+
+const ChangeCaseUtil = require('../utils/ChangeCaseUtil');
+const ModelBase = require('./ModelBase');
+
+
+class UserRequestModel extends ModelBase {
+    constructor(db, logger) {
+        super(db, logger, 'user_request', [
+            'id',
+            'isActivated',
+            'activatedTimestamp',
+            'speciality',
+            'email',
+            'firstName',
+            'lastName',
+            'telephone',
+            'company',
+            'loginType',
+            'password'
+        ]);
+    }
+
+    findInactiveAsync(id, trx) {
+        return trx.select()
+            .from(this.baseTableName)
+            .where('id', id)
+            .map((item) => this._mapColumns(item))
+            .then((items) => {
+                if (items && items.length) {
+                    const item = items[0];
+                    if (item.isActivated) {
+                        return Promise.reject('User is already activated');
+                    }
+                    return item;
+                } else {
+                    return Promise.reject('User is not found.');
+                }
+            });
+    }
+
+    activateAsync(id, email, trx) {
+        return this.findInactiveAsync(id, trx)
+            .then((item) => trx(this.baseTableName)
+                .where('id', item.id)
+                .update(ChangeCaseUtil.convertKeysToSnakeCase({
+                    isActivated: true,
+                    email
+                })));
+    }
+
+    createAsync(userInfo, trx) {
+        return trx(this.baseTableName)
+            .insert(
+                ChangeCaseUtil.convertKeysToSnakeCase(
+                    Object.assign({}, userInfo, {id: Uuid.v4(), isActivated: false}))
+                );
+    }
+}
+
+module.exports = UserRequestModel;
---
+++ registration/services/UserRequestService.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+const NUMBER_OF_PAID_SAMPLES = 0;
+
+class UserRequestService {
+    constructor(db, userRequestModel, usersClient) {
+        this.db = db;
+        this.userRequestModel = userRequestModel;
+        this.usersClient = usersClient;
+    }
+
+    activateAsync(registrationCodeId, firstName, lastName, userEmail) {
+        const {db, userRequestModel, usersClient} = this;
+
+        return db.transactionallyAsync((trx) =>
+            userRequestModel.findInactiveAsync(registrationCodeId, trx)
+                .then(({speciality}) =>
+                    usersClient.addAsync('en', {firstName, lastName, userEmail, speciality, numberOfPaidSamples: NUMBER_OF_PAID_SAMPLES})
+                )
+                .then(() => userRequestModel.activateAsync(registrationCodeId, userEmail, trx))
+        );
+    }
+
+    createAsync(userInfo) {
+        const {db, userRequestModel} = this;
+        return db.transactionallyAsync((trx) =>
+            userRequestModel.createAsync(userInfo, trx));
+    }
+}
+
+module.exports = UserRequestService;
