4f7334a473435d98aa077c0e2b805d9600bd2a04
dmitry <dmitry.zyryanov@execution.su>
2016-10-03 14:33:29 +0700

    registration codes service form: squashed migrations, db creation and migration scripts

--- registration/database/migrations/20160926201536_remove-userinfo.js
+++
@@ -1,8 +0,0 @@
-
-exports.up = function(knex, Promise) {
-    return knex.schema.dropTable('user_info');
-};
-
-exports.down = function(knex, Promise) {
-    throw new Error('Can not restore user_info table');
-};
