14dc90cd50e2ef31e6832d7a4c8c0463b7cbb398
Vasily Loginov <loginov.vasily@gmail.com>
2016-10-12 14:39:12 +0700

    Added cors. Still working.

--- examples/upload/index.js
+++ examples/upload/index.js
@@ -1,18 +1,25 @@
 var express = require('express'),
     multer  = require('multer'),
-    fs = require('fs')
-var upload = multer({ dest: 'uploads/' })
+    fs = require('fs'),
+    cors = require('cors')
+    ;
+var upload = multer({ dest: 'uploads/' });
 
-var app = express()
+var app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
 app.use('/', express.static('.'));
 
 app.post('/', upload.single('file'), function(req, res) {
     const {body, file} = req;
-    console.log(body) // form fields
-    console.log(file) // form files
-    fs.unlinkSync(`${__dirname}/${file.path}`)
-    res.status(204).end()
+    console.log(body); // form fields
+    console.log(file); // form files
+    //fs.unlinkSync(`${__dirname}/${file.path}`);
+    res.status(204).end();
 });
 
 app.listen(3000);
