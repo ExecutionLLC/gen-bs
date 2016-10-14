c0c57af920ca610c659cf25cb583ae3c9592337b
Vasily Loginov <loginov.vasily@gmail.com>
2016-10-14 15:00:46 +0700

    Fixed name to be more descriptive.

--- scripts/create-db-backup.sh
+++ scripts/create-db-backup.sh
@@ -6,7 +6,7 @@ PG_PORT=${PG_PORT:-5432}
 PG_HOST=${PG_HOST:-localhost}
 PG_USER=${PG_USER:-postgres}
 PG_DATABASE=${PG_DATABASE:-genomixdb}
-PG_BACKUP_PATH=${PG_BACKUP_PATH:-./database-backup-${DATE_POSTFIX}.sql.gz}
+PG_BACKUP_PATH=${PG_BACKUP_PATH:-./backup-${PG_DATABASE}-${DATE_POSTFIX}.sql.gz}
 
 pg_dump --port=${PG_PORT} -h ${PG_HOST} -U ${PG_USER} ${PG_DATABASE} | gzip > ${PG_BACKUP_PATH}
 
