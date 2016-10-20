6fe34c0d86cbdebb5eafd3f5028e386a4acc4eb1
Vasily Loginov <loginov.vasily@gmail.com>
2016-10-20 12:09:15 +0700

    Added migration making all emails lowercased.

---
+++ database/migrations/20161020115602_email-lower-case.js
@@ -0,0 +1,8 @@
+exports.up = function(knex, Promise) {
+    console.log('Making all emails lower case');
+    return knex.raw('update "user" set email = lower(email)');
+};
+
+exports.down = function(knex, Promise) {
+    throw new Error('Not implemented.')
+};
