fe808b4aa5ab5bb12acbe65685f915f8a9835209
Vasily Loginov <loginov.vasily@gmail.com>
2016-10-20 14:20:56 +0700

    Fixed migration to trim spaces from emails.

--- database/migrations/20161020115602_email-lower-case.js
+++ database/migrations/20161020115602_email-lower-case.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
     console.log('Making all emails lower case');
-    return knex.raw('update "user" set email = lower(email)');
+    return knex.raw('update "user" set email = trim(both from lower(email))');
 };
 
 exports.down = function(knex, Promise) {
