c55364a9a3e395509bb29a23cdbf89463c657122
dmitry <dmitry.zyryanov@execution.su>
2016-10-27 15:41:42 +0700

    unused badge removed

--- frontend/app/components/Header/NavbarMain/SamplesAnalysisButtons.js
+++ frontend/app/components/Header/NavbarMain/SamplesAnalysisButtons.js
@@ -29,7 +29,7 @@ export default class SamplesAnalysisButtons extends Component {
                     ><span
                         className='hidden-xxs'
                         data-localize='samples.title'
-                    >Samples</span><span className='visible-xxs'><i className='md-i'>file_upload</i></span>{/*<span class='badge badge-inverse'>2</span>*/}</a>
+                    >Samples</span><span className='visible-xxs'><i className='md-i'>file_upload</i></span></a>
                 </div>
                 <div>
                     <a
