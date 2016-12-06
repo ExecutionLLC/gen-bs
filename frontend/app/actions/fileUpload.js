7b1bf9381748d8893c0b6c718cc21f3d5d68fde5
dmitry <dmitry.zyryanov@execution.su>
2016-12-06 14:32:04 +0700

    upload without jquery, code review: old code removed

--- frontend/app/actions/fileUpload.js
+++ frontend/app/actions/fileUpload.js
@@ -154,8 +154,7 @@ function receiveFileOperation(upload, id) {
 }
 
 function sendFile(file, onOperationId, onProgress, onError) {
-
-    return sampleUploadsClient.upload(
+    sampleUploadsClient.upload(
         file,
         onProgress,
         (err, res) => {
@@ -166,38 +165,6 @@ function sendFile(file, onOperationId, onProgress, onError) {
             }
         }
     );
-/*
-    const formData = new FormData();
-    formData.append('sample', file);
-    formData.append('fileName', file.name);
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            let resp;
-            if (request.status === 200) {
-                try {
-                    resp = JSON.parse(request.response);
-                } catch (err) { }
-            }
-            if (!resp) {
-                onError(new Error('Invalid upload response'));
-            } else {
-                onOperationId(resp.upload);
-            }
-        }
-    };
-    if (request.upload) {
-        request.upload.addEventListener('progress', (progress) => {
-            const percentage = Math.floor((progress.loaded / progress.total) * 100);
-            if (percentage === 100) {
-                console.log('sendFile DONE!');
-            }
-            onProgress(percentage);
-        });
-    }
-    request.open('POST', config.URLS.FILE_UPLOAD);
-    request.send(formData);
-*/
 }
 
 export function changeFileUploadProgressState(progressValue, progressStatus, id) {
