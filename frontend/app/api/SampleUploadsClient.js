0ae89be122991ebca1813de2cd1725604983a584
dmitry <dmitry.zyryanov@execution.su>
2016-12-06 14:25:32 +0700

    upload without jquery, code review: xhr replaced by superagent's request

--- frontend/app/api/SampleUploadsClient.js
+++ frontend/app/api/SampleUploadsClient.js
@@ -1,3 +1,4 @@
+import RequestWrapper from './RequestWrapper';
 import UserEntityClientBase from './UserEntityClientBase';
 
 import config from '../../config';
@@ -8,42 +9,13 @@ export default class SampleUploadsClient extends UserEntityClientBase {
     }
 
     upload(file, onProgress, onComplete) {
-
-        const formData = new FormData();
-        formData.append('sample', file);
-        formData.append('fileName', file.name);
-        const request = new XMLHttpRequest();
-        request.onreadystatechange = () => {
-            if (request.readyState === 4) {
-                let resp;
-                if (request.status === 200) {
-                    try {
-                        resp = JSON.parse(request.response);
-                    } catch (err) { }
-                }
-                if (!resp) {
-                    onComplete(new Error('Invalid upload response'));
-                } else {
-                    onComplete(null, resp.upload);
-                }
-            }
-        };
-        if (request.upload) {
-            request.upload.addEventListener('progress', (progress) => {
-                const percentage = Math.floor((progress.loaded / progress.total) * 100);
-                if (percentage === 100) {
-                    console.log('sendFile DONE!');
-                }
-                onProgress(percentage);
-            });
-        }
-        request.open('POST', config.URLS.FILE_UPLOAD);
-        request.send(formData);
-
-        function breakUpload() {
-            console.log('breakUpload');
-        }
-
-        return breakUpload;
+        RequestWrapper.upload(
+            config.URLS.FILE_UPLOAD,
+            null,
+            null,
+            file,
+            onProgress,
+            onComplete
+        );
     }
 }
