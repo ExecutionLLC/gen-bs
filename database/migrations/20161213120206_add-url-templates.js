afb1af0df3b78b1d2723962bd869d3963b8e5640
andrey.ableev <andre.ableev@execution.su>
2016-12-14 14:58:02 +0700

    Fixed search criteria for the entries being updated.

--- database/migrations/20161213120206_add-url-templates.js
+++ database/migrations/20161213120206_add-url-templates.js
@@ -7,34 +7,50 @@ exports.up = function(knex, Promise) {
     const url_templates = [
         {
             field_name: 'INFO_CLNACC',
+            value_type: 'string',
+            dimension: 0,
             url: 'https://www.ncbi.nlm.nih.gov/clinvar/'
         },
         {
             field_name: 'VEP_SYMBOL',
+            value_type: 'string',
+            dimension: 0,
             url: 'http://www.genenames.org/cgi-bin/gene_symbol_report?match='
         },
         {
             field_name: 'VEP_Gene',
+            value_type: 'string',
+            dimension: 0,
             url: 'https://www.ncbi.nlm.nih.gov/gene?cmd=Retrieve&dopt=full_report&list_uids='
         },
         {
             field_name: 'VEP_Feature',
+            value_type: 'string',
+            dimension: 0,
             url: 'https://www.ncbi.nlm.nih.gov/nuccore/'
         },
         {
             field_name: 'VEP_HGNC_ID',
+            value_type: 'string',
+            dimension: 0,
             url: 'http://www.genenames.org/cgi-bin/gene_symbol_report?hgnc_id='
         },
         {
             field_name: 'VEP_PUBMED',
+            value_type: 'string',
+            dimension: 0,
             url: 'https://www.ncbi.nlm.nih.gov/pubmed/'
         },
         {
             field_name: 'VEP_HGVSc_transcript',
+            value_type: 'string',
+            dimension: 0,
             url: 'https://www.ncbi.nlm.nih.gov/nuccore/'
         },
         {
             field_name: 'VEP_HGVSp_transcript',
+            value_type: 'string',
+            dimension: 0,
             url: 'https://www.ncbi.nlm.nih.gov/protein/'
         }
     ];
@@ -44,7 +60,11 @@ exports.up = function(knex, Promise) {
             return;
         }
         return knex('field_metadata')
-            .where('name', item.field_name)
+            .where({
+                'name': item.field_name,
+                'value_type': item.value_type,
+                'dimension': item.dimension
+            })
             .update({
                 is_hyperlink: true,
                 hyperlink_template: item.url + defaultLinkIdentity
