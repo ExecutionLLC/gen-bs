b5ae4170f0a605029d7960fefd63103a6a6c4194
ponomarevvl90 <ponomarev.vl90@gmail.com>
2017-01-25 18:01:21 +0700

    add ru samples

---
+++ database/migrations/20170124180008_add-ruslanguage-data/samples.js
@@ -0,0 +1,39 @@
+const rusSamples = [
+    {
+        fileName: 'Breast Cancer Patient VCF File',
+        ruName: 'VCF Файл пациента с раком груди',
+        ruDescription: ''
+    },
+    {
+        fileName: 'Retinoblastoma Patient VCF File',
+        ruName: 'VCF Файл пациента с ретинобластомой',
+        ruDescription: ''
+    },
+    {
+        fileName: 'Whole Exome Sequencing VCF File',
+        ruName: 'Полная экзомная последовательность',
+        ruDescription: ''
+    },
+    {
+        fileName: 'Patient\'s VCF File',
+        ruName: 'VCF Файл пациента',
+        ruDescription: ''
+    },
+    {
+        fileName: 'Father',
+        ruName: 'Отец',
+        ruDescription: ''
+    },
+    {
+        fileName: 'Mother',
+        ruName: 'Мать',
+        ruDescription: ''
+    },
+    {
+        fileName: 'Proband',
+        ruName: 'Образец',
+        ruDescription: ''
+    }
+];
+
+exports.rusSamples = rusSamples;
