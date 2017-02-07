76295230b88af2ba836a8cfd615bd6598f6c2ab9
andrey.ableev <andre.ableev@execution.su>
2017-02-07 19:40:59 +0700

    #881: update user's default language (fixed existing tests).

--- test/mocks/MockUsersController.js
+++ test/mocks/MockUsersController.js
@@ -6,6 +6,7 @@ class MockUsersController {
     constructor(usersController) {
         this.originalUsersController = usersController;
         this.originalAdd = usersController.add;
+        this.originalUpdate = usersController.update;
         this.originalCreateRouter = usersController.createRouter;//.bind(usersController);
         usersController.add = this.add.bind(this);
         usersController.update = this.update.bind(this);
@@ -18,24 +19,12 @@ class MockUsersController {
     }
 
     update(request, response) {
-        async.waterfall([
-            (callback) => this.getRequestBody(request, callback),
-            (item, callback) => {
-                const languId = request.languId;
-                const {user, key} = item;
-                if (key === this.config.regserver.ADD_USER_KEY) {
-                    this.services.users.update(user.id, languId, user, callback);
-                } else {
-                    callback('Invalid update user key');
-                }
-            }
-        ], (error, insertedItem) => {
-            this.sendErrorOrJson(response, error, insertedItem);
-        });
+        this.originalUpdate.call(this.originalUsersController, request, response);
     }
 
     createRouter() {
         const router = this.originalCreateRouter.call(this.originalUsersController);
+        router.post('/', this.add.bind(this.originalUsersController));
         router.put('/', this.update.bind(this.originalUsersController));
         return router;
     }
