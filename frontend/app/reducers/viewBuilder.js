9b423449c7ab7ce0db294f4d48aa91ec6e6dee18
dmitry <dmitry.zyryanov@execution.su>
2017-02-20 14:04:20 +0700

    refactor TODOs, view builder: reducers extracted

--- frontend/app/reducers/viewBuilder.js
+++ frontend/app/reducers/viewBuilder.js
@@ -3,6 +3,15 @@ import _ from 'lodash';
 import * as ActionTypes from '../actions/viewBuilder';
 import {entityType} from '../utils/entityTypes';
 import * as i18n from '../utils/i18n';
+import immutableArray from '../utils/immutableArray';
+
+const INITIAL_STATE = {
+    editingView: null,
+    originalView: null,
+    editingViewIsNew: false,
+    editingViewParentId: '',
+    allowedFields: null
+};
 
 function getNextDirection(direction) {
     if (!direction) {
@@ -52,140 +61,143 @@ function reduceVBuilderSaveEdit(state) {
 }
 
 function reduceVBuilderEndEdit(state) {
-    return Object.assign({} ,state, {
-        editingView: null,
-        originalView: null,
-        editingViewIsNew: false,
-        editingViewParentId: '',
-        allowedFields: null
+    return {
+        ...state,
+        ...INITIAL_STATE
+    };
+}
+
+function reduceVBuilderDeleteColumn(state, action) {
+    return {
+        ...state,
+        editingView: {
+            ...state.editingView,
+            viewListItems: immutableArray.remove(state.editingView.viewListItems, action.viewItemIndex)
+        }
+    };
+}
+
+function reduceVBuilderAddColumn(state, action) {
+    const newViewItem = createViewItem(action.columnFieldId);
+    return {
+        ...state,
+        editingView: {
+            ...state.editingView,
+            viewListItems: immutableArray.replace(state.editingView.viewListItems, action.viewItemIndex, newViewItem)
+        }
+    };
+}
+
+function reduceVBuilderChangeAttr(state, action) {
+    return {
+        ...state,
+        editingView: i18n.changeEntityText(
+            state.editingView,
+            action.languageId,
+            {
+                name: action.name,
+                description: action.description
+            }
+        )
+    };
+}
+
+function reduceVBuilderChangeColumn(state, action) {
+    const changedViewItem = createViewItem(action.fieldId);
+    return {
+        ...state,
+        editingView: {
+            ...state.editingView,
+            viewListItems: immutableArray.assign(state.editingView.viewListItems, action.viewItemIndex, changedViewItem)
+        }
+    };
+}
+
+function reduceVBuilderChangeSortColumn(state, action) { // TODO refactor
+    const viewItems = [...state.editingView.viewListItems];
+    const firstSortItemIndex = _.findIndex(viewItems, {sortOrder: 1});
+    const secondSortItemIndex = _.findIndex(viewItems, {sortOrder: 2});
+    const selectedSortItemIndex = _.findIndex(viewItems, {fieldId: action.fieldId});
+    const selectedOrder = action.sortOrder;
+    const selectedDirection = getNextDirection(action.sortDirection);
+    if (selectedSortItemIndex == secondSortItemIndex || selectedSortItemIndex == firstSortItemIndex) {
+        viewItems[selectedSortItemIndex] = Object.assign({}, viewItems[selectedSortItemIndex], {
+            sortDirection: selectedDirection
+        });
+        if (selectedDirection == null) {
+            viewItems[selectedSortItemIndex] = Object.assign({}, viewItems[selectedSortItemIndex], {
+                sortOrder: null
+            });
+            if (selectedSortItemIndex == firstSortItemIndex && secondSortItemIndex != -1) {
+                viewItems[secondSortItemIndex] = Object.assign({}, viewItems[secondSortItemIndex], {
+                    sortOrder: 1
+                });
+            }
+        }
+    } else {
+        const oldSortItemIndex = _.findIndex(viewItems, {sortOrder: selectedOrder});
+        if (oldSortItemIndex != -1) {
+            viewItems[oldSortItemIndex] = Object.assign({}, viewItems[oldSortItemIndex], {
+                sortOrder: null,
+                sortDirection: null
+            });
+        }
+        viewItems[selectedSortItemIndex] = Object.assign({}, viewItems[selectedSortItemIndex], {
+            sortDirection: selectedDirection,
+            sortOrder: firstSortItemIndex == -1 ? 1 : selectedOrder
+        });
+    }
+    return Object.assign({}, state, {
+        editingView: Object.assign({}, state.editingView, {
+            viewListItems: viewItems
+        })
     });
 }
 
+function reduceVBuilderSetItemKeywords(state, action) {
+    return {
+        ...state,
+        editingView: {
+            ...state.editingView,
+            viewListItems: immutableArray(state.editingView.viewListItems, action.viewItemIndex, {keywords: action.keywordsIds})
+        }
+    };
+}
+
+function reduceVBuilderOnSave(state, action) {
+    return {
+        ...state,
+        onSaveAction: action.onSaveAction,
+        onSaveActionProperty: action.onSaveActionProperty
+    };
+}
+
 export default function viewBuilder(state = {
-    editingView: null,
-    originalView: null,
-    editingViewIsNew: false,
-    editingViewParentId: '',
-    allowedFields: null,
+    ...INITIAL_STATE,
     isFetching: false
 }, action) {
 
-    switch (action.type) { // TODO extract reducers
+    switch (action.type) {
         case ActionTypes.VBUILDER_START_EDIT:
             return reduceVBuilderStartEdit(state, action);
         case ActionTypes.VBUILDER_SAVE_EDIT:
             return reduceVBuilderSaveEdit(state);
         case ActionTypes.VBUILDER_END_EDIT:
             return reduceVBuilderEndEdit(state);
-        case ActionTypes.VBUILDER_DELETE_COLUMN: {
-            return Object.assign({}, state, {
-                editingView: Object.assign({}, state.editingView, {
-                    viewListItems: [
-                        ...state.editingView.viewListItems.slice(0, action.viewItemIndex),
-                        ...state.editingView.viewListItems.slice(action.viewItemIndex + 1)
-                    ]
-                })
-            });
-        }
-        case ActionTypes.VBUILDER_ADD_COLUMN: {
-            const newViewItem = createViewItem(action.columnFieldId);
-            return Object.assign({}, state, {
-                editingView: Object.assign({}, state.editingView, {
-                    viewListItems: [
-                        ...state.editingView.viewListItems.slice(0, action.viewItemIndex),
-                        newViewItem,
-                        ...state.editingView.viewListItems.slice(action.viewItemIndex)
-                    ]
-                })
-            });
-        }
-        case ActionTypes.VBUILDER_CHANGE_ATTR: {
-            return Object.assign({}, state, {
-                editingView: i18n.changeEntityText(
-                    state.editingView,
-                    action.languageId,
-                    {
-                        name: action.name,
-                        description: action.description
-                    }
-                )
-            });
-        }
-        case ActionTypes.VBUILDER_CHANGE_COLUMN: {
-            const changedViewItem = createViewItem(action.fieldId);
-            return Object.assign({}, state, {
-                editingView: Object.assign({}, state.editingView, {
-                    viewListItems: [
-                        ...state.editingView.viewListItems.slice(0, action.viewItemIndex),
-
-                        Object.assign({}, state.editingView.viewListItems[action.viewItemIndex], changedViewItem),
-
-                        ...state.editingView.viewListItems.slice(action.viewItemIndex + 1)
-                    ]
-                })
-            });
-        }
-        case ActionTypes.VBUILDER_CHANGE_SORT_COLUMN: {
-            const viewItems = [...state.editingView.viewListItems];
-            const firstSortItemIndex = _.findIndex(viewItems, {sortOrder: 1});
-            const secondSortItemIndex = _.findIndex(viewItems, {sortOrder: 2});
-            const selectedSortItemIndex = _.findIndex(viewItems, {fieldId: action.fieldId});
-            const selectedOrder = action.sortOrder;
-            const selectedDirection = getNextDirection(action.sortDirection);
-            if (selectedSortItemIndex == secondSortItemIndex || selectedSortItemIndex == firstSortItemIndex) {
-                viewItems[selectedSortItemIndex] = Object.assign({}, viewItems[selectedSortItemIndex], {
-                    sortDirection: selectedDirection
-                });
-                if (selectedDirection == null) {
-                    viewItems[selectedSortItemIndex] = Object.assign({}, viewItems[selectedSortItemIndex], {
-                        sortOrder: null
-                    });
-                    if (selectedSortItemIndex == firstSortItemIndex && secondSortItemIndex != -1) {
-                        viewItems[secondSortItemIndex] = Object.assign({}, viewItems[secondSortItemIndex], {
-                            sortOrder: 1
-                        });
-                    }
-                }
-            } else {
-                const oldSortItemIndex = _.findIndex(viewItems, {sortOrder: selectedOrder});
-                if (oldSortItemIndex != -1) {
-                    viewItems[oldSortItemIndex] = Object.assign({}, viewItems[oldSortItemIndex], {
-                        sortOrder: null,
-                        sortDirection: null
-                    });
-                }
-                viewItems[selectedSortItemIndex] = Object.assign({}, viewItems[selectedSortItemIndex], {
-                    sortDirection: selectedDirection,
-                    sortOrder: firstSortItemIndex == -1 ? 1 : selectedOrder
-                });
-            }
-            return Object.assign({}, state, {
-                editingView: Object.assign({}, state.editingView, {
-                    viewListItems: viewItems
-                })
-            });
-        }
-        case ActionTypes.VBUILDER_SET_ITEM_KEYWORDS: {
-            return Object.assign({}, state, {
-                editingView: Object.assign({}, state.editingView, {
-                    viewListItems: [
-                        ...state.editingView.viewListItems.slice(0, action.viewItemIndex),
-
-                        Object.assign({}, state.editingView.viewListItems[action.viewItemIndex], {
-                            keywords: action.keywordsIds
-                        }),
-
-                        ...state.editingView.viewListItems.slice(action.viewItemIndex + 1)
-                    ]
-                })
-            });
-        }
+        case ActionTypes.VBUILDER_DELETE_COLUMN:
+            return reduceVBuilderDeleteColumn(state, action);
+        case ActionTypes.VBUILDER_ADD_COLUMN:
+            return reduceVBuilderAddColumn(state, action);
+        case ActionTypes.VBUILDER_CHANGE_ATTR:
+            return reduceVBuilderChangeAttr(state, action);
+        case ActionTypes.VBUILDER_CHANGE_COLUMN:
+            return reduceVBuilderChangeColumn(state, action);
+        case ActionTypes.VBUILDER_CHANGE_SORT_COLUMN:
+            return reduceVBuilderChangeSortColumn(state, action);
+        case ActionTypes.VBUILDER_SET_ITEM_KEYWORDS:
+            return reduceVBuilderSetItemKeywords(state, action);
         case ActionTypes.VBUILDER_ON_SAVE:
-            return {
-                ...state,
-                onSaveAction: action.onSaveAction,
-                onSaveActionProperty: action.onSaveActionProperty
-            };
+            return reduceVBuilderOnSave(state, action);
         default:
             return state;
     }
