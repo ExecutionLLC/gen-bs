1c44abda97a73497fb8fd0db84aa150820d38585
dmitry <dmitry.zyryanov@execution.su>
2017-02-20 11:02:53 +0700

    refactor variants table: reducers extracted

--- frontend/app/reducers/variantsTable.js
+++ frontend/app/reducers/variantsTable.js
@@ -27,181 +27,212 @@ const initialState = {
     selectedRowIndices: []
 };
 
-export default function variantsTable(state = initialState, action) {
-    switch (action.type) { // TODO extract reducers
+function reduceClearSearchParams(state) {
+    return Object.assign({}, state, {
+        searchInResultsParams: DEFAULT_SEARCH_PARAMS
+    });
+}
 
-        case ActionTypes.CLEAR_SEARCH_PARAMS: {
-            return Object.assign({}, state, {
-                searchInResultsParams: DEFAULT_SEARCH_PARAMS
-            });
-        }
+function reduceChangeVariantsLimit(state) {
+    return Object.assign({}, state, {
+        searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
+            offset: state.searchInResultsParams.offset + state.searchInResultsParams.limit
+        }),
+        isNextDataLoading: true,
+        isFetching: true
+    });
+}
 
-        case ActionTypes.CHANGE_VARIANTS_LIMIT: {
-            return Object.assign({}, state, {
-                searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
-                    offset: state.searchInResultsParams.offset + state.searchInResultsParams.limit
-                }),
-                isNextDataLoading: true,
-                isFetching: true
-            });
-        }
-        case ActionTypes.CHANGE_VARIANTS_GLOBAL_FILTER: {
-            const currentGlobalSearchString = state.searchInResultsParams.topSearch.search;
-            if (currentGlobalSearchString === action.globalSearchString) {
+function reduceChangeVariantsGlobalFilter(state, action) {
+    const currentGlobalSearchString = state.searchInResultsParams.topSearch.search;
+    if (currentGlobalSearchString === action.globalSearchString) {
+        return state;
+    }
+    return Object.assign({}, state, {
+        searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
+            topSearch: {
+                search: action.globalSearchString
+            },
+            ...DEFAULT_SEARCH_PARAMS_LIMIT_OFFSET
+        }),
+        needUpdate: true
+    });
+}
+
+function reduceSetFieldFilter(state, action) {
+    // copy search array
+    var searchArray = [...state.searchInResultsParams.search];
+    const fieldIndex = _.findIndex(searchArray, {fieldId: action.fieldId, sampleId: action.sampleId});
+
+    if (action.filterValue !== '') {
+        if (fieldIndex !== -1) {
+            const currentFilterValue = searchArray[fieldIndex].value;
+            if (currentFilterValue === action.filterValue) {
+                // filter value is the same
                 return state;
             }
-            return Object.assign({}, state, {
-                searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
-                    topSearch: {
-                        search: action.globalSearchString
-                    },
-                    ...DEFAULT_SEARCH_PARAMS_LIMIT_OFFSET
-                }),
-                needUpdate: true
-            });
+            // update current filter
+            searchArray[fieldIndex].value = action.filterValue;
+        } else {
+            // it is new filter
+            searchArray.push({fieldId: action.fieldId, sampleId: action.sampleId, value: action.filterValue});
         }
-        case ActionTypes.SET_FIELD_FILTER: {
-            // copy search array
-            var searchArray = [...state.searchInResultsParams.search];
-            const fieldIndex = _.findIndex(searchArray, {fieldId: action.fieldId, sampleId: action.sampleId});
+    } else {
+        // filter value is empty, so we should remove filter
+        if (fieldIndex > -1) {
+            searchArray.splice(fieldIndex, 1);
+        }
+    }
 
-            if (action.filterValue !== '') {
-                if (fieldIndex !== -1) {
-                    const currentFilterValue = searchArray[fieldIndex].value;
-                    if (currentFilterValue === action.filterValue) {
-                        // filter value is the same
-                        return state;
-                    }
-                    // update current filter
-                    searchArray[fieldIndex].value = action.filterValue;
-                } else {
-                    // it is new filter
-                    searchArray.push({fieldId: action.fieldId, sampleId: action.sampleId, value: action.filterValue});
-                }
-            } else {
-                // filter value is empty, so we should remove filter
-                if (fieldIndex > -1) {
-                    searchArray.splice(fieldIndex, 1);
-                }
+    return Object.assign({}, state, {
+        searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
+            search: searchArray,
+            ...DEFAULT_SEARCH_PARAMS_LIMIT_OFFSET
+        }),
+        needUpdate: true
+    });
+}
+
+function reduceSetVariantsSort(state, action) {
+    return Object.assign({}, state, {
+        searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
+            sort: action.sortOrder
+        })
+    });
+}
+
+function reduceChangeVariantsSort(state, action) {
+    // copy sort array
+    var sortArray = [...state.searchInResultsParams.sort];
+    var fieldIndex = _.findIndex(sortArray, {fieldId: action.fieldId, sampleId: action.sampleId});
+
+    if (fieldIndex === -1) {
+        // it is new column for sorting
+        const newItem = {fieldId: action.fieldId, sampleId: action.sampleId, direction: action.sortDirection};
+        if (sortArray.length < action.sortOrder) {
+            // put new item to the end of array
+            fieldIndex = sortArray.length; // FIXME will be pushed?
+        } else {
+            // replace existent item, which has the same order
+            fieldIndex = action.sortOrder - 1;
+            // remove sorting with higher order
+            // NOTE: if you want to save state of the sorting with higher order, then
+            // just remove next line
+            sortArray = sortArray.slice(0, fieldIndex);
+        }
+        sortArray[fieldIndex] = newItem;
+    } else {
+        // user clicked on the column and want to remove sorting or change direction
+        // NOTE: we do not allow to change order of the existent items, because it
+        // is clashed with change direction, remove sorting logic
+        if (sortArray[fieldIndex].direction !== action.sortDirection) {
+            // user want to change sort direction
+            sortArray[fieldIndex].direction = action.sortDirection;
+        } else {
+            // user want to remove sorting
+            if (sortArray.length === 1) {
+                // we do not allow remove all sorting
+                return state;
             }
+            sortArray.splice(fieldIndex, 1);
+        }
+    }
+    // update sort order parameter
+    // NOTE: actually I don't understand why we hold order in attribute of the obj
+    // (our array already hold order as index)
+    for (var i = 0; i < sortArray.length; i++) {
+        sortArray[i].order = i + 1;
+    }
 
-            return Object.assign({}, state, {
-                searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
-                    search: searchArray,
-                    ...DEFAULT_SEARCH_PARAMS_LIMIT_OFFSET
-                }),
-                needUpdate: true
-            });
-        }
-        case ActionTypes.SET_VARIANTS_SORT: {
-            return Object.assign({}, state, {
-                searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
-                    sort: action.sortOrder
-                })
-            });
-        }
-        case ActionTypes.CHANGE_VARIANTS_SORT: {
-            // copy sort array
-            var sortArray = [...state.searchInResultsParams.sort];
-            var fieldIndex = _.findIndex(sortArray, {fieldId: action.fieldId, sampleId: action.sampleId});
+    return Object.assign({}, state, {
+        searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
+            sort: sortArray,
+            ...DEFAULT_SEARCH_PARAMS_LIMIT_OFFSET
+        }),
+        needUpdate: true
+    });
+}
 
-            if (fieldIndex === -1) {
-                // it is new column for sorting
-                const newItem = {fieldId: action.fieldId, sampleId: action.sampleId, direction: action.sortDirection};
-                if (sortArray.length < action.sortOrder) {
-                    // put new item to the end of array
-                    fieldIndex = sortArray.length; // FIXME will be pushed?
-                } else {
-                    // replace existent item, which has the same order
-                    fieldIndex = action.sortOrder - 1;
-                    // remove sorting with higher order
-                    // NOTE: if you want to save state of the sorting with higher order, then
-                    // just remove next line  
-                    sortArray = sortArray.slice(0, fieldIndex);
-                }
-                sortArray[fieldIndex] = newItem;
-            } else {
-                // user clicked on the column and want to remove sorting or change direction
-                // NOTE: we do not allow to change order of the existent items, because it
-                // is clashed with change direction, remove sorting logic
-                if (sortArray[fieldIndex].direction !== action.sortDirection) {
-                    // user want to change sort direction
-                    sortArray[fieldIndex].direction = action.sortDirection;
-                } else {
-                    // user want to remove sorting
-                    if (sortArray.length === 1) {
-                        // we do not allow remove all sorting
-                        return state;
-                    }
-                    sortArray.splice(fieldIndex, 1);
-                }
-            }
-            // update sort order parameter
-            // NOTE: actually I don't understand why we hold order in attribute of the obj
-            // (our array already hold order as index)
-            for (var i = 0; i < sortArray.length; i++) {
-                sortArray[i].order = i + 1;
-            }
+function reduceRequestVariants(state) {
+    return Object.assign({}, state, {
+        isFetching: true
+    });
+}
 
-            return Object.assign({}, state, {
-                searchInResultsParams: Object.assign({}, state.searchInResultsParams, {
-                    sort: sortArray,
-                    ...DEFAULT_SEARCH_PARAMS_LIMIT_OFFSET
-                }),
-                needUpdate: true
-            });
-        }
-        case ActionTypes.REQUEST_VARIANTS: {
-            return Object.assign({}, state, {
-                isFetching: true
-            });
-        }
-        case ActionTypes.RECEIVE_ANALYSIS_OPERATION_ID: {
-            return Object.assign({}, state, {
-                isFetching: false,
-                operationId: action.operationId,
-                lastUpdated: action.receivedAt
-            });
-        }
+function reduceReceiveAnalysisOperationId(state, action) {
+    return Object.assign({}, state, {
+        isFetching: false,
+        operationId: action.operationId,
+        lastUpdated: action.receivedAt
+    });
+}
 
-        case ActionTypes.REQUEST_SEARCHED_RESULTS: {
-            return Object.assign({}, state, {
-                isNextDataLoading: action.isNextDataLoading,
-                isFilteringOrSorting: action.isFilteringOrSorting,
-                isFetching: true,
-                needUpdate: false
-            });
-        }
-        case ActionTypes.RECEIVE_SEARCHED_RESULTS: {
-            return Object.assign({}, state, {
-                isNextDataLoading: false,
-                isFilteringOrSorting: false,
-                isFetching: false,
-                lastUpdated: action.receivedAt,
-                isReceivedAll: action.isReceivedAll
-            });
-        }
-        case ActionTypes.SELECT_VARIANTS_ROW: {
-            const {rowIndex, isSelected} = action;
-            const {selectedRowIndices} = state;
-            let newSelectedRowIndices;
-            if (isSelected) {
-                newSelectedRowIndices = selectedRowIndices.concat([rowIndex]);
-            } else {
-                newSelectedRowIndices = _.filter(selectedRowIndices, item => item !== rowIndex);
-            }
+function reduceRequestSearchedResults(state, action) {
+    return Object.assign({}, state, {
+        isNextDataLoading: action.isNextDataLoading,
+        isFilteringOrSorting: action.isFilteringOrSorting,
+        isFetching: true,
+        needUpdate: false
+    });
+}
 
-            return Object.assign({}, state, {
-                selectedRowIndices: newSelectedRowIndices
-            });
-        }
+function reduceReceiveSearchedResults(state, action) {
+    return Object.assign({}, state, {
+        isNextDataLoading: false,
+        isFilteringOrSorting: false,
+        isFetching: false,
+        lastUpdated: action.receivedAt,
+        isReceivedAll: action.isReceivedAll
+    });
+}
 
-        case ActionTypes.CLEAR_VARIANTS_ROWS_SELECTION: {
-            return Object.assign({}, state, {
-                selectedRowIndices: initialState.selectedRowIndices
-            });
-        }
+function reduceSelectVariantsRow(state, action) {
+    const {rowIndex, isSelected} = action;
+    const {selectedRowIndices} = state;
+    let newSelectedRowIndices;
+    if (isSelected) {
+        newSelectedRowIndices = selectedRowIndices.concat([rowIndex]);
+    } else {
+        newSelectedRowIndices = _.filter(selectedRowIndices, item => item !== rowIndex);
+    }
 
+    return Object.assign({}, state, {
+        selectedRowIndices: newSelectedRowIndices
+    });
+}
+
+function reduceClearVariantsRowSelection(state) {
+    return Object.assign({}, state, {
+        selectedRowIndices: initialState.selectedRowIndices
+    });
+}
+
+export default function variantsTable(state = initialState, action) {
+    switch (action.type) {
+        case ActionTypes.CLEAR_SEARCH_PARAMS:
+            return reduceClearSearchParams(state);
+        case ActionTypes.CHANGE_VARIANTS_LIMIT:
+            return reduceChangeVariantsLimit(state);
+        case ActionTypes.CHANGE_VARIANTS_GLOBAL_FILTER:
+            return reduceChangeVariantsGlobalFilter(state, action);
+        case ActionTypes.SET_FIELD_FILTER:
+            return reduceSetFieldFilter(state, action);
+        case ActionTypes.SET_VARIANTS_SORT:
+            return reduceSetVariantsSort(state, action);
+        case ActionTypes.CHANGE_VARIANTS_SORT:
+            return reduceChangeVariantsSort(state, action);
+        case ActionTypes.REQUEST_VARIANTS:
+            return reduceRequestVariants(state);
+        case ActionTypes.RECEIVE_ANALYSIS_OPERATION_ID:
+            return reduceReceiveAnalysisOperationId(state, action);
+        case ActionTypes.REQUEST_SEARCHED_RESULTS:
+            return reduceRequestSearchedResults(state, action);
+        case ActionTypes.RECEIVE_SEARCHED_RESULTS:
+            return reduceReceiveSearchedResults(state, action);
+        case ActionTypes.SELECT_VARIANTS_ROW:
+            return reduceSelectVariantsRow(state, action);
+        case ActionTypes.CLEAR_VARIANTS_ROWS_SELECTION:
+            return reduceClearVariantsRowSelection(state);
         default:
             return state;
     }
