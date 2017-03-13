aefa84dcbbca769a3b3ddd72a435015cfb0f79c6
dmitry <dmitry.zyryanov@execution.su>
2017-03-13 18:59:35 +0700

    Startup frontend warnings #897: make id for table value popovers

--- frontend/app/components/VariantsTable/VariantsTableRow.js
+++ frontend/app/components/VariantsTable/VariantsTableRow.js
@@ -136,8 +136,10 @@ export default class VariantsTableRow extends ComponentBase {
     }
 
     renderPopupValue(ref, value, field, isValuedHyperlink, isChromosome) {
+        const {rowIndex} = this.props;
+        const id = `${rowIndex}-${ref}`; // unique id, 'ref' is unique for row, add rowIndex for full-table unique
         const popover = (
-            <Popover
+            <Popover id={id}
                 onClick={() => this.refs[ref].hide()}
             >
                 {isValuedHyperlink ? this.renderHyperLinks(field.hyperlinkTemplate, value) :
