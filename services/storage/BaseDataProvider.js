96fa1e7c2e72347d8c8894f147aae4c158377520
ponomarevvl90 <ponomarev.vl90@gmail.com>
2017-04-06 11:10:37 +0700

    add amazon and filesystem data providers

---
+++ services/storage/BaseDataProvider.js
@@ -0,0 +1,36 @@
+class BaseDataProvider{
+
+    constructor(config, logger) {
+        this._config = config;
+        this._logger = logger;
+    }
+
+    get logger() {
+        return this._logger;
+    }
+    get config() {
+        return this._config;
+    }
+
+    addSampleFile(fileName, fileStream, callback){
+        throw new Error('Not implemented.');
+    }
+
+    removeSampleFile(fileName, callback){
+        throw new Error('Not implemented.');
+    }
+
+    getSamplePath(fileName){
+        throw new Error('Not implemented.');
+    }
+
+    addSavedFile(fileName, fileStream, callback){
+        throw new Error('Not implemented.');
+    }
+
+    getSavedFile(fileName, callback){
+        throw new Error('Not implemented.');
+    }
+}
+
+module.exports = BaseDataProvider;
