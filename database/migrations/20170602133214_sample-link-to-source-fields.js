e09c3b0931737f0c03344663d92698af9e162aa6
ponomarevvl90 <ponomarev.vl90@gmail.com>
2017-06-02 16:12:30 +0700

    add source fields to all samples

---
+++ database/migrations/20170602133214_sample-link-to-source-fields.js
@@ -0,0 +1,47 @@
+const _ = require('lodash');
+const BluebirdPromise = require('bluebird');
+const ChangeCaseUtil = require('../../utils/ChangeCaseUtil');
+
+exports.up = function (knex) {
+    console.log('=> Add source field to sample ...');
+    return addSourceFieldToSample(knex)
+};
+
+exports.down = function (knex, Promise) {
+
+};
+
+function addSourceFieldToSample(knex) {
+    return findSourceFieldsIds(knex)
+        .then((fieldIds) => {
+            return findSampleGenotypesIds(knex)
+                .then((sampleIds) => {
+                    return BluebirdPromise.mapSeries(sampleIds, sampleId => {
+                        return BluebirdPromise.mapSeries(fieldIds, fieldId => {
+                            return addSampleField(knex, sampleId, fieldId);
+                        });
+                    });
+                })
+        });
+}
+
+function addSampleField(knex, sampleId, fieldId) {
+    return knex('sample_field')
+        .insert(ChangeCaseUtil.convertKeysToSnakeCase({
+            sampleId,
+            fieldId
+        }));
+}
+
+function findSourceFieldsIds(knex) {
+    return knex.select('id')
+        .from('field')
+        .whereNot('source_name', 'sample')
+        .then((results) => _.map(results, result => result.id));
+}
+
+function findSampleGenotypesIds(knex) {
+    return knex.select('id')
+        .from('sample')
+        .then((results) => _.map(results, result => result.id));
+}
